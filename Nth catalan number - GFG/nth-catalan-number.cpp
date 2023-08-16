f8fb059d325c2604f2743361258704be16f8c48d
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-08-16 13:41:17 +0530

    Added solution - LeetHub

---
+++ Nth catalan number - GFG/nth-catalan-number.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    
+    int findCatalan(int n) {
+        const int MOD = 1000000007;
+        vector<int> catalan(n + 1, 0);
+
+        catalan[0] = 1;
+        for (int i = 1; i <= n; i++){
+            for (int j = 0; j < i; j++){
+                catalan[i] = (catalan[i] + (1LL * catalan[j] * catalan[i - j - 1]) % MOD) % MOD;
+            }
+        }
+        return catalan[n];
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--) {
+	    
+	    int n;
+	    cin>>n;
+	    Solution obj;
+	    cout<< obj.findCatalan(n) <<"\n";    
+	}
+	return 0;
+}
+// } Driver Code Ends
