21c6e564fe9c1b3991286c43ff2374692e64684d
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-08-22 22:49:03 +0530

    Time: 2246 ms (5.03%), Space: 247.5 MB (9.92%) - LeetHub

---
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
@@ -0,0 +1,44 @@
+class Solution{
+    int ans;
+public:
+    
+    void f(int k, string &s, int &maxLetters){
+        
+        int n = s.size();
+        int i = 0;
+        int j = 0;
+        map<char, int> mpS;
+        map<string, int> mp;
+        string str;
+        
+        while(j < n){
+            
+            mpS[s[j]]++;
+            str += s[j];
+            if(j-i+1 < k) j++;
+            else{
+                
+                if(mpS.size() <= maxLetters){
+        
+                    mp[str]++;
+                    ans = max(ans, mp[str]);
+                    
+                }
+                mpS[s[i]]--;
+                str.erase(0, 1);
+                if(mpS[s[i]] == 0) mpS.erase(s[i]);
+                i++;
+                j++;
+            }
+        }
+    }
+    
+    int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
+        
+        ans = 0;
+        for(int i=minSize; i<=maxSize; i++){
+            f(i, s, maxLetters);
+        }
+        return ans;
+    }
+};
