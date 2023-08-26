ea9f8562aac37302f6026adec185c1671d39aa6b
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-08-26 10:09:36 +0530

    Added solution - LeetHub

--- Longest K unique characters substring - GFG/longest-k-unique-characters-substring.cpp
+++ Longest K unique characters substring - GFG/longest-k-unique-characters-substring.cpp
@@ -34,7 +34,7 @@ class Solution{
                     if(mp[s[i]] == 0) mp.erase(s[i]);
                     i++;
                 }
-                ans = max(ans, j-i+1);
+                // ans = max(ans, j-i+1);
                 j++;
             }
         }
