a01179beed0be483fcd38c7a5ffd9535e725af22
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-09-10 11:20:56 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (16.67%) - LeetHub

--- 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -6,15 +6,14 @@ public:
         int y = abs(sy-fy)+1;
         
         int sm = 0;
-        cout<<x<<' '<<y;
         if(x == 1 and y == 1) return t != 1;
-        if(x == 1 and y != 1) sm = y-1;
-        else if(x != 1 and y == 1) sm = x-1;
-        else if(x == y) sm = x-1;
-        else{
+        // if(x == 1 and y != 1) sm = y-1;
+        // else if(x != 1 and y == 1) sm = x-1;
+        // else if(x == y) sm = x-1;
+        // else{
             sm = min(x,y)-1;
             sm += max(x,y) - min(x,y);
-        }
+        // }
         return sm <= t;
     }
         
