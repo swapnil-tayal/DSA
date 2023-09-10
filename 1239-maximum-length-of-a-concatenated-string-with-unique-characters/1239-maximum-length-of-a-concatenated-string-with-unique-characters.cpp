85c71afe2b078a75758d26da17b2f44d64b7a6bd
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-09-10 16:15:08 +0530

    Time: 718 ms (8.50%), Space: 277.9 MB (9.15%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    int solve(vector<string> &arr, int n, string temp, map<string, int> &dp){
+        
+        set<int> st;
+        for(auto i: temp){
+            if(st.count(i)) return -1;
+            st.insert(i);
+        }
+        
+        if(n == -1){
+            return st.size();
+        }
+        
+        string str;
+        str += to_string(n);
+        str += temp;
+        
+        if(dp.count(str)) return dp[str];
+        
+        
+        int pick = solve(arr, n-1, temp+arr[n], dp);
+        int npick = solve(arr, n-1, temp, dp);
+        return dp[str] = max(pick, npick);
+    }
+    
+    int maxLength(vector<string>& arr) {
+        int n = arr.size();
+        map<string, int> dp;
+        return solve(arr, n-1, "", dp);
+    }
+};
