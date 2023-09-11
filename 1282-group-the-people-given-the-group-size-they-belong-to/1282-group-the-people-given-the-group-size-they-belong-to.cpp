22f26b66bb718ee42995c4af46f80b56baf531d1
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-09-11 09:22:55 +0530

    Time: 23 ms (7.50%), Space: 15.5 MB (5.29%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        
+        int n = groupSizes.size();
+        map<int, vector<vector<int>>> mp;
+        
+        for(int i=0; i<n; i++){
+            
+            int x = groupSizes[i];
+            if(mp.find(x) == mp.end()){
+                mp[x].push_back({i});
+            
+            }else{
+                if(mp[x].back().size() < x){
+                    mp[x].back().push_back(i);
+                
+                }else mp[x].push_back({i});
+            }
+        }
+        vector<vector<int>> ans;
+        for(auto i: mp){
+            for(auto j: i.second) ans.push_back(j);
+        }
+        return ans;
+    }
+};
