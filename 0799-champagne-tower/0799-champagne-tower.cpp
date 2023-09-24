078cfd2a26a11ee3d41a82c866ffd8eaca8efcd3
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-09-24 09:25:28 +0530

    Time: 20 ms (18.54%), Space: 24 MB (29.51%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
+    
+        vector<vector<double>> grid;
         
-        // if(poured >= 5050) return 1;
-        
-        vector<vector<double>> grid(101, vector<double>(100, 0));
-        grid[0][0] = (double)poured;
-        
+        vector<double> temp = {(double)poured};
+        grid.push_back(temp);
         
         for(int i=0; i<100; i++){
             
             int f = 0;
+            vector<double> temp(i+2, 0);
             for(int j=0; j<=i; j++){
                 
                 if(grid[i][j] > 1){
                     
                     double x = grid[i][j]-1;
                     grid[i][j] = 1;
-                    grid[i+1][j] += x/(2.0);
-                    grid[i+1][j+1] += x/(2.0);
+                    temp[j] += x/(2.0);
+                    temp[j+1] += x/(2.0);
                     f = 1;
                 }
             }
+            grid.push_back(temp);
             if(f == 0) break; 
         }
-        // for(int i=0; i<4; i++){
-        //     for(int j=0; j<=i; j++) cout<<grid[i][j]<<' ';
-        //     cout<<'\n';
-        // }
+        if(query_row >= grid.size()) return 0;
         return grid[query_row][query_glass];
     }
 };
