ff47249182d4f981a334265d1e61d337889e6e7e
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-09-25 09:10:49 +0530

    Time: 0 ms (100.00%), Space: 7 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        for(int i=0; i<s.size(); i++){
-            if(s[i] != t[i]) return t[i];
-        }
-        return t[t.size()-1];
+        char ch = 0;
+        for(auto i: s) ch ^= i;
+        for(auto i: t) ch ^= i;
+        return ch;
     }
 };
