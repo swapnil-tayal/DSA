55ac4b8c730ad3c2c264a12588297abaebf99103
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-09-26 10:51:45 +0530

    Time: 5 ms (5.93%), Space: 7.1 MB (5.19%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        
+        int n = s.size();
+        map<char, vector<int>> mp;
+        for(int i=0; i<s.size(); i++){
+            mp[s[i]].push_back(i);
+        }
+        
+        vector<int> vis(26, 0); 
+        stack<int> st;
+        string ans;
+        
+        for(int i=0; i<n; i++){
+            
+            if(vis[s[i]-'a'] == 1) continue;
+            
+            if(st.size() == 0){
+                st.push(s[i]);
+                vis[s[i]-'a'] = 1;
+            }
+            else{
+                
+                if(st.top() == s[i]) continue;
+                else if(st.top() < s[i]){
+                    st.push(s[i]);
+                    vis[s[i]-'a'] = 1;
+                }
+                else{
+                    
+                    if(i == 5) cout<<(char)st.top()<<' ';
+                    
+                    while(!st.empty() and st.top() > s[i]){
+                        char ch = st.top();
+                        int ind = upper_bound( mp[ch].begin(), mp[ch].end(), i ) - mp[ch].begin();
+                        
+                        if(ind != mp[ch].size()){
+                            vis[st.top()-'a'] = 0;
+                            st.pop();
+                        }
+                        else break;
+                    }
+                    st.push(s[i]);
+                    vis[s[i]-'a'] = 1;
+                }
+            }
+        }
+        string str;
+        while(!st.empty()){
+            str += st.top();
+            st.pop();
+        }
+        reverse(str.begin(), str.end());
+        return str;
+    }
+};
