d6a9d52c89d02f7d8a0682eece39d0387b603a28
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-10-14 10:38:37 +0530

    Time: 254 ms (20.28%), Space: 139.9 MB (13.42%) - LeetHub

---
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    int n;
+public:
+    
+    int f(int i, vector<int> &cost, vector<int> &time, int cnt, vector<vector<int>> &dp){
+        
+        if(i == time.size()){
+            if(cnt >= 0) return 0;
+            return 1e9;
+        }
+        if(dp[i][cnt+n] != -1) return dp[i][cnt+n];
+         
+        int pick = cost[i] + f(i+1, cost, time, min(n, cnt + time[i]), dp);
+        int npcik = f(i+1, cost, time, cnt-1, dp);
+        
+        return dp[i][cnt+n] =  min(pick, npcik);
+    }
+    
+    int paintWalls(vector<int>& cost, vector<int>& time) {
+        
+        this->n = cost.size();
+        vector<vector<int>> dp(n, vector<int>(2*n+10, -1));
+        return f(0, cost, time, 0, dp);
+    }
+};
