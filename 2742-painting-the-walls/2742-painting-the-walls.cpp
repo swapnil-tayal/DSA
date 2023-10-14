b354d7cbda31adb57c2a0e6a5a5ea02977390bae
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-10-14 10:55:00 +0530

    Time: 154 ms (62.56%), Space: 115.4 MB (61.00%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.cpp
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -4,22 +4,23 @@ public:
     
     int f(int i, vector<int> &cost, vector<int> &time, int cnt, vector<vector<int>> &dp){
         
+        if(cnt >= n) return 0;
         if(i == time.size()){
-            if(cnt >= 0) return 0;
+            if(cnt >= n) return 0;
             return 1e9;
         }
-        if(dp[i][cnt+n] != -1) return dp[i][cnt+n];
+        if(dp[i][cnt] != -1) return dp[i][cnt];
          
-        int pick = cost[i] + f(i+1, cost, time, min(n, cnt + time[i]), dp);
-        int npcik = f(i+1, cost, time, cnt-1, dp);
+        int pick = cost[i] + f(i+1, cost, time, 1 + cnt + time[i], dp);
+        int npcik = f(i+1, cost, time, cnt, dp);
         
-        return dp[i][cnt+n] =  min(pick, npcik);
+        return dp[i][cnt] =  min(pick, npcik);
     }
     
     int paintWalls(vector<int>& cost, vector<int>& time) {
         
         this->n = cost.size();
-        vector<vector<int>> dp(n, vector<int>(2*n+1, -1));
+        vector<vector<int>> dp(n, vector<int>(n, -1));
         return f(0, cost, time, 0, dp);
     }
 };
