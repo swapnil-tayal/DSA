19a043ef6bd1961deadb99d50b01d0de2f55c91c
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-10-25 19:39:24 +0530

    Time: 366 ms (52.88%), Space: 33.8 MB (50.96%) - LeetHub

--- 2911-minimum-changes-to-make-k-semi-palindromes/2911-minimum-changes-to-make-k-semi-palindromes.cpp
+++ 2911-minimum-changes-to-make-k-semi-palindromes/2911-minimum-changes-to-make-k-semi-palindromes.cpp
@@ -22,26 +22,26 @@ public:
         return ans;
     }
     
-    int f(int i, int prev, int k, string &s, vector<vector<vector<int>>> &dp, vector<vector<int>> &pre){
+    int f(int i, int k, string &s, vector<vector<int>> &dp, vector<vector<int>> &pre){
         
         if(i == n){
-            if(k == 0 and prev != n){
-                if(pre[prev][i-1] != -1) return pre[prev][i-1];
-                else return pre[prev][i-1] = num(s.substr(prev, i - prev + 1));
-            }
+            if(k == 0) return 0; 
             else return 1e9;
         }
-        if(dp[i][prev][k] != -1) return dp[i][prev][k];
+        if(dp[i][k] != -1) return dp[i][k];
         
-        int npick = f(i+1, prev, k, s, dp, pre);
         int pick = 1e9;
         if(k > 0){
-            int x;
-            if(pre[prev][i] != -1) x = pre[prev][i];
-            else x = pre[prev][i] = num(s.substr(prev, i - prev + 1));
-            pick = x + f(i+1, i+1, k-1, s, dp, pre);
+            for(int ind=i; ind<n; ind++){
+
+                int x;
+                if(pre[i][ind] != -1) x = pre[i][ind];
+                else x = pre[i][ind] = num(s.substr(i, ind - i + 1));
+                int cost = x + f(ind+1, k-1, s, dp, pre);
+                pick = min(cost, pick);
+            }
         }
-        return dp[i][prev][k] = min(pick, npick);
+        return dp[i][k] = pick;
     }
     
     int minimumChanges(string s, int k){
@@ -54,8 +54,8 @@ public:
                 }
             }
         }
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(k, -1)));
+        vector<vector<int>> dp(n, vector<int>(k+1, -1));
         vector<vector<int>> pre(n+10, vector<int> (n+10, -1));
-        return f(0, 0, k-1, s, dp, pre);
+        return f(0, k, s, dp, pre);
     }
 };
