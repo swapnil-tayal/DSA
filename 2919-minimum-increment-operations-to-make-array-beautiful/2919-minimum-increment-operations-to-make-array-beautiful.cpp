8541f3d4a19ef34c029579f59cc27e6911559f49
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-10-29 10:00:33 +0530

    Time: 250 ms (20.00%), Space: 146.5 MB (20.00%) - LeetHub

--- 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.cpp
+++ 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.cpp
@@ -3,30 +3,15 @@ public:
     
     long long f(int i, vector<int> &nums, int k, int cnt, vector<vector<long long>> &dp){
         
-        if(i == nums.size()){
-            // if(cnt == 0) return 1e9;
-            return 0;
-        }
+        if(i == nums.size()) return 0;
+        
         if(dp[i][cnt] != -1) return dp[i][cnt];
         
-        if(cnt == 2){
-            
-            long long pick = max(0, k-nums[i]) + f(i+1, nums, k, 2, dp);
-            long long npcik = f(i+1, nums, k, 1, dp);
-            return dp[i][cnt] = min(pick, npcik);
-            
-        }else if(cnt == 1){
-            
-            long long pick = max(0, k-nums[i]) + f(i+1, nums, k, 2, dp);
-            long long npcik = f(i+1, nums, k, 0, dp);
-            return dp[i][cnt] = min(pick, npcik);
-            
-        }else{
-            
-            long long pick = max(0, k-nums[i]) + f(i+1, nums, k, 2, dp);
-            return dp[i][cnt] = pick;
-        }
+        long long pick = max(0, k-nums[i]) + f(i+1, nums, k, 2, dp);
+        long long npick = 1e18;
+        if(cnt > 0) npick = f(i+1, nums, k, cnt-1, dp);
         
+        return dp[i][cnt] = min(pick, npick);
     }
     
     long long minIncrementOperations(vector<int>& nums, int k) {
