9a2d4d98b9fcce26c92c63cb808bb90bcdeabed4
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-10-31 09:21:05 +0530

    Time: 79 ms (85.72%), Space: 76.5 MB (58.75%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        
+        int n = pref.size();
+        vector<int> ans(n);
+        
+        ans[0] = pref[0];
+        int x = ans[0];
+        
+        for(int i=1; i<n; i++){
+            
+            ans[i] = x ^ pref[i];
+            x = pref[i];
+        }
+        return ans;
+    }
+};
