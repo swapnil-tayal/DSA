00a046f64a81ac42c1bc30f6dbae0e33203e99cc
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-11-14 09:44:51 +0530

    Time: 667 ms (30.37%), Space: 14 MB (40.41%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -3,16 +3,22 @@ public:
     
     int countPalindromicSubsequence(string s) {
         
-        map<int, vector<int>> mp;
+        unordered_map<int, vector<int>> mp;
         for(int i=0; i<s.size(); i++){
-            mp[s[i]].push_back(i);
+            
+            if(mp[s[i]].size() == 0){
+                mp[s[i]].push_back(i);
+                mp[s[i]].push_back(i);
+            }else{
+                mp[s[i]].back() = i;
+            }
         }
         
         int ans = 0;
         for(auto i: mp){
             vector<int> temp = i.second;
             int x = temp[0];
-            int y = temp[temp.size()-1];
+            int y = temp[1];
             
             set<char> st;
             for(int j=x+1; j<y; j++){
