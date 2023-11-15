67da96400aa224e4ee8e00dd47099697360d65f5
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-11-15 08:27:57 +0530

    Time: 89 ms (12.69%), Space: 51.8 MB (14.55%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -5,13 +5,13 @@ public:
         sort(arr.begin(), arr.end());
         int n = arr.size();
         arr[0] = 1;
+        int ans = 1;
         for(int i=1; i<n; i++){
             if(arr[i] - arr[i-1] > 1){
                 arr[i] = arr[i-1]+1;
             }
+            ans = max(ans, arr[i]);
         }
-        int ans = 0;
-        for(auto &i: arr) ans = max(ans, i);
         return ans;
     }
 };
