e8a82090fae0f8b714f36f9a188322ad7fbffd47
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-11-17 09:51:06 +0530

    Time: 183 ms (72.14%), Space: 96.5 MB (94.23%) - LeetHub

---
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            ans = max(ans, nums[i]+nums[n-i-1]);
+        }
+        return ans;
+    }
+};
