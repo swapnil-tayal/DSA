3345eb9dfcaa5e76ee5efe2fabc954dbdca09489
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-11-28 13:33:00 +0530

    Time: 1198 ms (9.14%), Space: 403.3 MB (8.57%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -7,27 +7,22 @@ public:
         if(str[i] == 'S') seat++;
         if(seat > 2) return 0;
         
-        if(i == str.size()-1){
-            return seat == 2;
-        }
+        if(i == str.size()-1) return seat == 2;
+        
         
         if(dp[i][seat] != -1) return dp[i][seat];
         
-        if(seat <= 1){
-            int ncut = f(i+1, seat, str, dp);
-            return dp[i][seat] = ncut;
-            
-        }else{
-            int cut = f(i+1, 0, str, dp);
-            int ncut = f(i+1, seat, str, dp);
-            return dp[i][seat] = (cut + ncut) % mod;
-        }
+        int cut = 0;
+        if(seat == 2) cut = f(i+1, 0, str, dp);
+        int ncut = f(i+1, seat, str, dp);
+        
+        return dp[i][seat] = (cut + ncut) % mod;
     }
     
     int numberOfWays(string str) {
         
         int n = str.size();
-        vector<vector<int>> dp(n, vector<int>(4, -1));
+        vector<vector<int>> dp(n, vector<int>(3, -1));
         return f(0, 0, str, dp);
     }
 };
