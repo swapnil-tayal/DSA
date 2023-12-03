29e22f164607da570d7816d029df05bd3740a5d0
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-12-03 09:22:44 +0530

    Time: 10 ms (33.18%), Space: 10.5 MB (64.64%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -8,7 +8,7 @@ public:
             
             int x = abs(points[i][0] - points[i-1][0]);
             int y = abs(points[i][1] - points[i-1][1]);
-            ans += min(x,y) + max(x,y)-min(x,y); 
+            ans += max(x,y);
         }
         return ans;
     }
