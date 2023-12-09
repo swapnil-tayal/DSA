91aaec0e4bd5624b3a4d059404bd4c6986166e0f
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-12-09 12:34:06 +0530

    Time: 120 ms (59.87%), Space: 98 MB (37.93%) - LeetHub

--- 2772-apply-operations-to-make-all-array-elements-equal-to-zero/2772-apply-operations-to-make-all-array-elements-equal-to-zero.cpp
+++ 2772-apply-operations-to-make-all-array-elements-equal-to-zero/2772-apply-operations-to-make-all-array-elements-equal-to-zero.cpp
@@ -4,21 +4,20 @@ public:
         
         int n = nums.size();
         int sum = 0;
+        vector<int> a(n+1, 0);
         unordered_map<int,int> mp;
         
         for(int i=0; i<n; i++){
             
-            if(mp.find(i) != mp.end()) sum -= mp[i];
+            sum -= a[i];
             nums[i] -= sum;
             if(nums[i] == 0) continue;
             if(nums[i] < 0) return 0;
             
             sum += nums[i];
-            mp[i+k] = nums[i];
+            if(i+k > n) return 0;
+            a[i+k] = nums[i];
             nums[i] = 0;
-        }
-        for(auto i: mp){
-            if(i.first > n) return 0;
         }
         return 1;
     }
