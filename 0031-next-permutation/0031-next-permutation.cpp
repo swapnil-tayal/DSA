a0ad58c25a720a66097af1c14faa277772474feb
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-12-16 15:09:58 +0530

    Time: 4 ms (52.06%), Space: 12.4 MB (91.97%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,6 +2,34 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         
-        next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+        if(n == 1) return;
+        int i = n-1;
+        int val = -1;
+        
+        while(i > 0){
+            if(nums[i] > nums[i-1]){
+                val = i-1;
+                break;
+            }i--;
+        }
+        if(val == -1){
+            sort(nums.begin(), nums.end());
+            return;
+        }
+        
+        int val2 = i;
+        int ac = n;
+        for(int k=i; k<n; k++){
+            if(nums[k] > nums[val]){
+                if(ac > nums[k] and nums[val] < nums[k]){
+                    ac = nums[k];
+                    val2 = k;
+                }
+            }
+        }
+        // cout<<val<<' '<<val2<<'\n';
+        swap(nums[val], nums[val2]);
+        sort(nums.begin()+val+1, nums.end());
     }
 };
