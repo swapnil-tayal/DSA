03b83af5c66148358e4f8278bbea2f0eee895a40
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-12-16 19:17:16 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (24.58%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -3,54 +3,42 @@ public:
     string removeDuplicateLetters(string s) {
         
         int n = s.size();
-        map<char, vector<int>> mp;
-        for(int i=0; i<s.size(); i++){
-            mp[s[i]].push_back(i);
+        map<char,int> mp;
+        for(int i=0; i<n; i++){
+            mp[s[i]] = i;
         }
         
-        vector<int> vis(26, 0); 
-        stack<int> st;
-        string ans;
+        stack<char> st;
+        vector<int> vis(26, 0);
         
         for(int i=0; i<n; i++){
             
-            if(vis[s[i]-'a'] == 1) continue;
-            
-            if(st.size() == 0){
+            if(vis[s[i]-'a']) continue;
+            if(st.size() == 0 || st.top() < s[i]){
+                
+                st.push(s[i]);
+                vis[s[i] - 'a'] = 1;
+                
+            }else{
+                
+                
+                while(!st.empty() and st.top() > s[i]){
+                    
+                    int ind = mp[st.top()];
+                    if(ind > i){
+                        
+                        vis[st.top()-'a'] = 0;
+                        st.pop();
+                    
+                    }else break;
+                }
                 st.push(s[i]);
                 vis[s[i]-'a'] = 1;
-            }
-            else{
-                
-                if(st.top() == s[i]) continue;
-                else if(st.top() < s[i]){
-                    st.push(s[i]);
-                    vis[s[i]-'a'] = 1;
-                }
-                else{
-                    
-                    while(!st.empty() and st.top() > s[i]){
-                        
-                        char ch = st.top();
-                        int ind = mp[ch].back();
-                        
-                        if(ind > i){
-                            vis[st.top()-'a'] = 0;
-                            st.pop();
-                        }
-                        else break;
-                    }
-                    st.push(s[i]);
-                    vis[s[i]-'a'] = 1;
-                }
             }
         }
         string str;
         while(!st.empty()){
-            string temp;
-            temp += st.top();
-            temp += str;
-            str = temp;
+            str = st.top() + str;
             st.pop();
         }
         return str;
