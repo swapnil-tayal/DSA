6c6988c0a3eb1cffa496bc44ee258c5742b11461
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-12-17 23:35:53 +0530

    Time: 50 ms (74.44%), Space: 13.6 MB (77.91%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -3,30 +3,30 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         
         sort(nums.begin(), nums.end());
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         int n = nums.size();
         
         for(int i=0; i<n; i++){
             
-            // if(i > 0 and nums[i] == nums[i-1]) continue;
+            if(i > 0 and nums[i] == nums[i-1]) continue;
             for(int j=i+1; j<n; j++){
                 
-                // if(j > 1 and nums[j] == nums[j-1]) continue;
+                if(j > i+1 and nums[j] == nums[j-1]) continue;
                 int x = j+1;
                 int y = n-1;
                 long long sm = nums[i] + nums[j];
                 
                 while(x < y){
                     
-                    if(sm + nums[x] + nums[y] < target){
+                    if(sm + nums[x] + nums[y] < target || (x > j+2 and nums[x] == nums[x-1])){
                         x++;
                     }
-                    else if(sm + nums[x] + nums[y] > target){
+                    else if(sm + nums[x] + nums[y] > target || (y < n-1 and nums[y] == nums[y+1])){
                         y--;
                     }
                     else{
                         
-                        ans.insert({nums[i], nums[j], nums[x], nums[y]});
+                        ans.push_back({nums[i], nums[j], nums[x], nums[y]});
                         int t = nums[x];
                         int t2 = nums[y];
                         y--;
@@ -35,7 +35,7 @@ public:
                 }
             }
         }
-        vector<vector<int>> res (ans.begin(), ans.end());
-        return res;
+        // vector<vector<int>> res (ans.begin(), ans.end());
+        return ans;
     }
 };
