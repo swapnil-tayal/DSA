e3a3faae06e59c8f2a6eeb9ea1a313a64e583999
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-12-17 20:57:40 +0530

    Time: 15 ms (51.53%), Space: 20.1 MB (28.45%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()/2];
+        
+        int n = nums.size();
+        int major = nums[0];
+        int count = 1;
+        for(int i=1; i<n; i++){
+            
+            if(count == 0){
+                major = nums[i];
+                count = 1;
+            
+            }else if(major == nums[i]){
+                count++;
+            
+            }else count--;
+        }
+        return major;
     }
 };
