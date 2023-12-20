5659176906188f071458d6023a18a05c8a00d241
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-12-20 19:14:52 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (5.88%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.c
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.c
@@ -1,37 +1,39 @@
 char* removeDuplicateLetters(char* s) {
     
     int n = strlen(s);
+    char* st = (char*)malloc((1000000)*sizeof(char));
+    int ind = -1;
+    
     int mp[26] = {0};
+    int vis[26] = {0};
     for(int i=0; i<n; i++){
         mp[s[i]-'a'] = i;
     }
     
-    char* res = (char*)malloc((n+1)*sizeof(char));
-    int resSize = 0;
-    int vis[26] = {0};
-    
     for(int i=0; i<n; i++){
         
-        if(vis[s[i] - 'a']) continue;
-        if(resSize == 0 || res[resSize-1] < s[i]){
-            
-            res[resSize++] = s[i];
+        if(vis[s[i]-'a']) continue;
+        // printf("%c\n", s[i]);
+        
+        if(ind == -1 || st[ind] < s[i]){
+            st[++ind] = s[i];
             vis[s[i]-'a'] = 1;
         
         }else{
             
-            while(resSize > 0 && res[resSize-1] > s[i]){
+            while(ind >= 0 && st[ind] >= s[i]){
                 
-                int ind = mp[res[resSize-1]-'a'];
-                if(ind > i){
-                    vis[res[--resSize]-'a'] = 0;
-                    
+                int k = mp[st[ind] - 'a'];
+                if(k > i){
+                    // printf("%c ", st[ind]);
+                    vis[st[ind] - 'a'] = 0;
+                    ind--;
                 }else break;
             }
-            res[resSize++] = s[i];
+            st[++ind] = s[i];
             vis[s[i]-'a'] = 1;
         }
     }
-    res[resSize] = '\0';
-    return res;
+    st[ind+1] = '\0';
+    return st;
 }
