ea9ed2d429df4340bbcab18e2e8d0ec1e8130352
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-12-20 15:27:24 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (61.50%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.c
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -1,41 +1,24 @@
-int size = -1;
-char arr[10000];
-
-char top(){
-    if(size == -1) return '.';
-    return arr[size];
-}
-char pop(){
-    if(size == -1) return -1;
-    return arr[size--];
-}
-void push(char ch){
-    arr[++size] = ch;
-}
-bool isEmpty(){
-    if(size == -1) return true;
-    return false;
-}
-bool isEqual(char ch1, char ch2){
+int f(char a, char b){
     
-    if(ch1 == '(' && ch2 == ')') return 0;
-    if(ch1 == '{' && ch2 == '}') return 0;
-    if(ch1 == '[' && ch2 == ']') return 0;
-    return 1;
+    if( (a == '(' && b == ')') ||   
+        (a == '[' && b == ']') ||
+        (a == '{' && b == '}') ) return false;
+    return true;
 }
+
 bool isValid(char* s) {
     
-    size = -1;
-    int n = strlen(s);
-    int i = 0;
+    char st[10001];
+    int ind = -1;
     
-    while(i < n){
-        // printf("%c %c\n", top(), s[i]);
-        if(isEmpty() || isEqual(top(), s[i])){
-            push(s[i]);
+    int n = strlen(s);
+    for(int i=0; i<n; i++){
         
-        }else pop();
-        i++;
+        if(ind == -1 || f(st[ind], s[i])){
+            st[++ind] = s[i];
+        }else{
+            ind--;
+        }
     }
-    return isEmpty();
+    return ind == -1;
 }
