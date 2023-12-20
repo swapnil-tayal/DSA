68adb2cec1c0b4d097cbcdf448ca83adec1a6267
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-12-20 21:55:11 +0530

    Time: 104 ms (48.14%), Space: 57.9 MB (48.13%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -3,8 +3,9 @@ public:
     int findKthLargest(vector<int>& nums, int k) {
         
         priority_queue<int, vector<int>, greater<int>> pq;
-        for(auto &i: nums){
-            pq.push(i);
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(pq.size() <= k) pq.push(nums[i]);
             if(pq.size() > k) pq.pop();
         }
         return pq.top();
