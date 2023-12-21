84c09755db63cced20462c087c2eadcb9f697ad9
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-12-21 10:43:42 +0530

    Time: 271 ms (15.38%), Space: 29.3 MB (23.08%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.c
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.c
@@ -1,35 +1,39 @@
-void merger(int* arr, int s, int mid, int e){
+// void merger(int* arr, int s, int mid, int e){
     
-    int* temp = (int*)malloc(sizeof(int) * (e-s+2));
+//     int* temp = (int*)malloc(sizeof(int) * (e-s+2));
     
-    int i = s;
-    int j = mid+1;
-    int k = 0;
-    while(i <= mid && j <= e){
+//     int i = s;
+//     int j = mid+1;
+//     int k = 0;
+//     while(i <= mid && j <= e){
         
-        if(arr[i] <= arr[j]){
-            temp[k++] = arr[i++]; 
-        }else{
-            temp[k++] = arr[j++];
-        }
-    }
-    while(i <= mid) temp[k++] = arr[i++];
-    while(j <= e) temp[k++] = arr[j++];
+//         if(arr[i] <= arr[j]){
+//             temp[k++] = arr[i++]; 
+//         }else{
+//             temp[k++] = arr[j++];
+//         }
+//     }
+//     while(i <= mid) temp[k++] = arr[i++];
+//     while(j <= e) temp[k++] = arr[j++];
     
-    k = 0;
-    int ind = s;
-    while(k < (e-s+1)){
-        arr[ind++] = temp[k++];
-    }
-}
+//     k = 0;
+//     int ind = s;
+//     while(k < (e-s+1)){
+//         arr[ind++] = temp[k++];
+//     }
+// }
 
-void merge(int* arr, int s, int e){
+// void merge(int* arr, int s, int e){
     
-    if(s >= e) return;
-    int mid = (s+e)/2;
-    merge(arr, s, mid);
-    merge(arr, mid+1, e);
-    merger(arr, s, mid, e);
+//     if(s >= e) return;
+//     int mid = (s+e)/2;
+//     merge(arr, s, mid);
+//     merge(arr, mid+1, e);
+//     merger(arr, s, mid, e);
+// }
+
+int cmp (int * a, int * b) {
+   return *(int*)a - *(int*)b;
 }
 
 int maxWidthOfVerticalArea(int** points, int pointsSize, int* pointsColSize) {
@@ -39,8 +43,13 @@ int maxWidthOfVerticalArea(int** points, int pointsSize, int* pointsColSize) {
         arr[i] = points[i][0];
     }
     
-    merge(arr, 0, pointsSize-1);
+    // merge(arr, 0, pointsSize-1);
+    qsort(arr, pointsSize, sizeof(int), cmp);
     int ans = 0;
+    
+    for(int i=0; i<pointsSize; i++){
+        printf("%d ", arr[i]);
+    }
     
     for(int i=1; i<pointsSize; i++){
         if(arr[i] - arr[i-1] > ans){
