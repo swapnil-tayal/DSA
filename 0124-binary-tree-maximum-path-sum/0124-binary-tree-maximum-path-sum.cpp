45997a8ca49c6f0ce6f81b92cfe340bd3c9f9b68
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-12-23 11:20:38 +0530

    Time: 32 ms (5.72%), Space: 28.7 MB (11.04%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -10,41 +10,23 @@
  * };
  */
 class Solution {
+    int ans = -1e9;
 public:
     
-    int f(TreeNode* root, int x, map<pair<TreeNode*,int>, int> &dp){
+    int f(TreeNode* root){
         
-        if(!root){
-            if(x == 0) return -1e9;
-            return 0;
-        }
-        if(dp.find({root, x}) != dp.end()) return dp[{root, x}];
+        if(!root) return 0;
         
-        if(x == 0){
-            
-            int npick1 = f(root->left, 0, dp);
-            int npick2 = f(root->right, 0, dp);
-            // int pick1 = root->val + f(root->left, 1, dp);
-            // int pick2 = root->val + f(root->right, 1, dp);
-            int pick3 = root->val + f(root->left, 1, dp) + f(root->right, 1, dp);
-            return dp[{root, x}] = max({pick3, npick1, npick2});
-            
-        }else if(x == 1){
-            
-            int pick1 = root->val + f(root->left, 1, dp);
-            int pick2 = root->val + f(root->right, 1, dp);
-            // int pick3 = root->val + f(root->left, 1, dp) + f(root->right, 1, dp);
-            int npick = f(root->left, 2, dp) + f(root->right, 2, dp);
-            return dp[{root, x}] = max({pick1, pick2, npick});
-            
-        }else{
-            return dp[{root, x}] = 0;
-        }
+        int left = f(root->left);
+        int right = f(root->right);
+        
+        int maxCurr = max({ root->val, max(left, right) + root->val });
+        ans = max({ans, maxCurr, root->val + left + right});
+        return maxCurr;
     }
     
     int maxPathSum(TreeNode* root) {
-        
-        map<pair<TreeNode*,int>, int> dp;
-        return f(root, 0, dp);
+        f(root);
+        return ans;
     }
-}; 
+};
