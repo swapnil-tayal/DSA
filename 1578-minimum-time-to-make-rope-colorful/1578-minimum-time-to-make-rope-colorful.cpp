98e86a4d798d425aa78cb380bd91e66a0ab755af
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-12-27 09:11:50 +0530

    Time: 126 ms (34.43%), Space: 95.8 MB (37.26%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -11,18 +11,17 @@ public:
             
             char ch = colors[i];
             int cnt = 0;
-            priority_queue<int, vector<int>, greater<int>> pq;
+            int maxCost = 0;
+            int totalCost = 0;
             
             while(i < n && colors[i] == ch){
-                pq.push(neededTime[i]);
+                maxCost = max(maxCost, neededTime[i]);
+                totalCost += neededTime[i];
                 cnt++;
                 i++;
             }
             if(cnt >= 2){
-                while(pq.size() > 1){
-                    ans += pq.top();
-                    pq.pop();
-                }
+                ans += totalCost - maxCost;
             }
         }
         return ans;
