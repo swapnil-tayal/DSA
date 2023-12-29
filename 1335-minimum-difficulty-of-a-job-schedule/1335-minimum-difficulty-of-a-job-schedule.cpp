709c0cf127d9e18fe77ec457ff727d97a39fe0fe
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-12-29 09:46:49 +0530

    Time: 92 ms (25.06%), Space: 8.4 MB (15.90%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     
-    int f(int i, int d, int maxi, vector<int> &arr, vector<vector<vector<int>>> &dp){
+    int f(int i, int d, vector<int> &arr, vector<vector<int>> &dp){
         
         if(i == arr.size()){
-            if(d == 1) return maxi;
+            if(d == 0) return 0;
             else return 1e9;
         }
-        if(dp[i][d][maxi] != -1) return dp[i][d][maxi];
-        
-        int notBreak = f(i+1, d, max(maxi, arr[i]), arr, dp);
-        int breakPoint = 1e9; 
-        if(d >= 2 and i != 0){
-            breakPoint = maxi + f(i+1, d-1, arr[i], arr, dp);
+        if(dp[i][d] != -1) return dp[i][d];
+        if(d == 0) return 1e9;
+        int ans = 1e9;
+        int maxi = 0;
+        for(int k=i; k<arr.size(); k++){
+            maxi = max(maxi, arr[k]);
+            int cut = maxi + f(k+1, d-1, arr, dp);
+            ans = min(ans, cut);
         }
-        return dp[i][d][maxi] = min(notBreak, breakPoint);
+        return dp[i][d] = ans;
     }
     
     int minDifficulty(vector<int>& jobDifficulty, int d) {
         
         int n = jobDifficulty.size();
-        if(n < d) return -1;
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(d+1, vector<int>(1001, -1)));
-        return f(0, d, 0, jobDifficulty, dp);
+        vector<vector<int>> dp(n, vector<int>(d+1, -1));
+        int ans = f(0, d, jobDifficulty, dp); 
+        return ans == 1e9 ? -1 : ans;
     }
 };
