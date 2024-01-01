c0e5f76a015ae8ee9caaba68a6b78848fd7defd7
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-01-01 09:06:24 +0530

    Time: 45 ms (8.42%), Space: 17.7 MB (75.56%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        
+        int i = 0;
+        int j = 0;
+        int n = g.size();
+        int m = s.size();
+        
+        int cnt = 0;
+        while(i < n && j < m){
+            
+            if(g[i] <= s[j]){
+                i++;
+                j++;
+                cnt++;
+            }else{
+                j++;
+            }
+        }
+        return cnt;
+    }
+};
