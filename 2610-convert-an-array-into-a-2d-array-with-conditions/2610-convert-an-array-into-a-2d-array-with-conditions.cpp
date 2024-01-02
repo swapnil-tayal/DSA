06ecb36ceac2c598c5102e6d2697b7e884a5597a
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-01-02 09:17:18 +0530

    Time: 0 ms (100.00%), Space: 30 MB (62.82%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -2,30 +2,19 @@ class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
         
-        priority_queue<pair<int,int>> pq;
         map<int,int> mp;
-        for(auto &i: nums){
-            mp[i]++;
-        }
-        for(auto &i: mp){
-            pq.push({ i.second, i.first });
-        }
+        for(auto &i: nums) mp[i]++;
         
         vector<vector<int>> ans;
-        while(!pq.empty()){
+        for(auto &i: mp){
             
-            int cnt = pq.top().first;
-            int val = pq.top().second;
-            pq.pop();
-            if(ans.size() == 0){
-                for(int i=0; i<cnt; i++){
-                    ans.push_back({ val });
-                }
-            }else{
-                for(int i=0; i<cnt; i++){
-                    ans[i].push_back(val);
-                }
+            int cnt = i.second;
+            int val = i.first;
+            for(int i=0; i<cnt; i++){
+                if(ans.size() > i) ans[i].push_back(val);
+                else ans.push_back({ val });
             }
+            
         }
         return ans;
     }
