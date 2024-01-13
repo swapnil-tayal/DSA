4a1da68e811035270d5cb07fb02e433f2859de19
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-01-13 10:19:14 +0530

    Time: 113 ms (19.23%), Space: 17 MB (62.45%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        
+        map<char,int> mp;
+        for(auto &i: s) mp[i]++;
+        for(auto &i: t) mp[i]--;
+        int ans = 0;
+        for(auto i: mp) ans += abs(i.second);
+        return ans/2;
+    }
+};
