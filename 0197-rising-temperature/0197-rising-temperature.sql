9017e00ec784de2d9205111036cf1271a0202ada
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-01-16 15:04:11 +0530

    Time: 1171 ms (17.92%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,3 +1,2 @@
-# Write your MySQL query statement below
-SELECT w1.id from Weather w1 join Weather w2 where DATEDIFF(w1.recordDate, w2.recordDate)=1 
-and w1.temperature>w2.temperature
+SELECT w1.id FROM Weather w1 JOIN Weather w2 WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND 
+w1.temperature > w2.temperature;
