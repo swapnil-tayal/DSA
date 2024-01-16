d3f45486472510d9db96d0c449c3fed508ee33a7
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-01-16 14:36:56 +0530

    Time: 2764 ms (28.63%), Space: 0B (100.00%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,4 +1,2 @@
-# Write your MySQL query statement below
-SELECT Visits.customer_id, COUNT(Visits.visit_id) AS count_no_trans FROM Visits LEFT JOIN Transactions
-ON Visits.visit_id = Transactions.visit_id WHERE Transactions.amount IS null
-GROUP BY Visits.customer_id;
+SELECT DISTINCT customer_id, COUNT(customer_id) AS count_no_trans FROM Visits 
+WHERE visit_id NOT IN ( SELECT visit_id FROM Transactions ) GROUP BY customer_id;
