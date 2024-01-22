4c7e90ae47a25dd97afa0d432dd2639d9618b3e4
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-01-22 19:38:22 +0530

    Time: 55 ms (26.11%), Space: 34.3 MB (8.31%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -4,7 +4,7 @@ public:
         
         int n = nums.size();
         
-        map<int, int> mp;
+        unordered_map<int, int> mp;
         
         for(int i=0; i<n; i++) mp[nums[i]]++;
         
