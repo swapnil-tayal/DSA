14412cf53913b163fdbdf9e7af0dafeba5059130
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-01-25 15:13:59 +0530

    Time: 555 ms (42.58%), Space: 189.1 MB (17.40%) - LeetHub

--- 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
+++ 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
@@ -1,29 +1,27 @@
 class Solution {
-    multiset<long long> l,r;
 public:
     long long minimumCost(vector<int>& nums, int k, int dist) {
         
+        long long ans = 1e16;
         int n = nums.size();
-        long long ans = 1e15;
-        k--;
+        multiset<long long> l, r;
         
+        k--;
         int i = 1;
         int j = 1;
         long long sum = 0;
         
         while(j < n){
             
-
             if(j-i+1 <= dist){
-                sum += nums[j];
                 l.insert(nums[j]);
+                sum += nums[j];
                 j++;
-            }
-            else{
+            }else{
                 
-                if(l.size() and nums[j] < *l.begin()){
-                    sum += nums[j];
+                if(nums[j] < *l.rbegin()){
                     l.insert(nums[j]);
+                    sum += nums[j];
                 }else r.insert(nums[j]);
                 
                 while(l.size() >= k){
@@ -31,9 +29,6 @@ public:
                     r.insert(*l.rbegin());
                     l.erase(l.find(*l.rbegin()));
                 }
-                
-//                 // cout<<i<<' '<<j<<'\n';
-                
                 while(l.size() < k and r.size()){
                     sum += *r.begin();
                     l.insert(*r.begin());
@@ -41,16 +36,15 @@ public:
                 }
                 ans = min(ans, sum);
                 
-                //erase
+                // erase
                 if(l.find(nums[i]) != l.end()){
                     sum -= nums[i];
                     l.erase(l.find(nums[i]));
-                    
-                }else if(r.find(nums[i]) != r.end()) r.erase(r.find(nums[i]));
-                
+                }else if(r.find(nums[i]) != r.end()){
+                    r.erase(r.find(nums[i]));
+                }
                 i++;
                 j++;
-            
             }
         }
         return ans + nums[0];
