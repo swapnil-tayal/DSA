30974ba4992aacd46ed9f851b0034307710af37b
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-01-25 09:52:54 +0530

    Time: 68 ms (18.27%), Space: 25.7 MB (6.35%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,19 +3,39 @@ public:
     
     int f(int i, int j, string &str1, string &str2, vector<vector<int>> &dp){
         
-        if(i == str1.size() || j == str2.size()) return 0;
+        if(i == -1 || j == -1) return 0;
         if(dp[i][j] != -1) return dp[i][j];
         int pick = 0;
         if(str1[i] == str2[j]){
-            pick = 1 + f(i+1, j+1, str1, str2, dp);
+            pick = 1 + f(i-1, j-1, str1, str2, dp);
         }
-        int npcik = max(f(i+1, j, str1, str2, dp), f(i, j+1, str1, str2, dp));
+        int npcik = max(f(i-1, j, str1, str2, dp), f(i, j-1, str1, str2, dp));
         return dp[i][j] = max(pick, npcik);
     }
     
     int longestCommonSubsequence(string text1, string text2) {
         
         vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
-        return f(0, 0, text1, text2, dp);
+        
+        string str;
+        int i = text1.size()-1, j = text2.size()-1;
+        int ans = f(text1.size()-1, text2.size()-1, text1, text2, dp);
+        
+//         while(i >= 0 and j >= 0){
+            
+//             if(text1[i] == text2[j]){
+//                 str = text1[i] + str;
+//                 i--; j--;
+//             }else{
+                
+//                 if(j-1 == -1) i--;
+//                 else if(i-1 == -1) j--;
+//                 else if(dp[i-1][j] == dp[i][j]) i--;
+//                 else j--;
+//             }
+//         }
+        // cout<<str.size()<<' ';
+        // cout<<str<<'\n';
+        return ans;
     }
 };
