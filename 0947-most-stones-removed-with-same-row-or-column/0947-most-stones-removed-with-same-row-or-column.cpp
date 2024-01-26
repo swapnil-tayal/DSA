b8bbbaee6e780c162e2d0fd3276d1c3b588cad46
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-01-26 18:56:22 +0530

    Time: 41 ms (64.81%), Space: 25.4 MB (9.82%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,45 +1,23 @@
-class DisjoinSet{
-
-    vector<int> parent;
-    vector<int> rank;
+class DSU{
     vector<int> size;
+    vector<int> parent;
     public:
-
-    DisjoinSet(int n){
-        parent.resize(n+1);
-        rank.resize(n+1, 0);
-        size.resize(n+1, 1);
-        for(int i=0; i<n; i++){
+    DSU(int n){
+        size.resize(n+1, 0);
+        parent.resize(n+1, 0);
+        for(int i=0; i<=n; i++){
             parent[i] = i;
-            rank[i] = 0;
         }
     }
     int findParent(int n){
-        if(parent[n] == n){
-            return n;
-        }
+        if(parent[n] == n) return n;
         return parent[n] = findParent(parent[n]);
     }
-    void unionByRank(int u, int v){
-
-        u = findParent(u);
-        v = findParent(v);
-        if(u == v)return;
-        if(rank[v] < rank[u]){
-            parent[v] = u;
-        }else if(rank[u] < rank[v]){
-            parent[u] = v;
-        }else{
-            parent[v] = u;
-            rank[u]++;
-        }
-    }
-    void unionBySize(int u, int v){
-
-        u = findParent(u);
-        v = findParent(v);
-        if(u == v)return;
-        if(size[v] < size[u]){
+    void unionBySize(int x, int y){
+        int u = findParent(x);
+        int v = findParent(y);
+        if(u == v) return;
+        if(size[u] > size[v]){
             parent[v] = u;
             size[u] += size[v];
         }else{
@@ -47,43 +25,33 @@ class DisjoinSet{
             size[v] += size[u];
         }
     }
-
 };
 
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
         
-        int n = 0;
-        int m = 0;
+        int row = 0;
+        int col = 0;
         for(auto i: stones){
-            n = max(n, i[0]);
-            m = max(m, i[1]);
+            row = max(row, i[0]);
+            col = max(col, i[1]);
         }
-        // cout<<n<<' '<<m<<'\n';
-        DisjoinSet *dj = new DisjoinSet(n+m+2); 
-        map<int,int> mp, mp2;
+        DSU *ds = new DSU(row + col + 2);
+        set<int> having;
         
-        for(auto &i: stones){
+        for(auto i: stones){
             
-            int row = i[0];
-            int col = i[1] + n + 1;
-            dj->unionBySize(row, col);
-            mp[row] = 1;
-            mp[col] = 1;
+            int r = i[0];
+            int c = i[1];
+            ds->unionBySize(r, c + row + 1);
+            having.insert(r);
+            having.insert(c+row+1);
         }
         set<int> st;
-        for(auto i: mp){
-            st.insert(dj->findParent(i.first));
+        for(auto i: having){
+            st.insert(ds->findParent(i));
         }
         return stones.size() - st.size();
     }
 };
-
-
-
-
-
-
-
-
