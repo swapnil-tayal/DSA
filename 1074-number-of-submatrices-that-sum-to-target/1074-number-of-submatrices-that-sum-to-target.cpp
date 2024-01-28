283b5c6729ca2a24582f4afd29498075d24a2eb0
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-01-28 11:36:13 +0530

    Time: 415 ms (93.12%), Space: 165.7 MB (41.03%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     
-    int subarraySum(vector<int>& nums, int k) {
-        
-        map<int,int> mp;
-        int n = nums.size();
-        int sum = 0;
-        mp[0] = 1;
-        int cnt = 0;
-        
-        for(int i=0; i<n; i++){
-            
-            sum += nums[i];
-            if(mp.find(sum - k) != mp.end()){
-                cnt += mp[sum-k];
-            }
-            mp[sum]++;
-        }
-        return cnt;
-    }
-    
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
         
         int n = matrix.size();
@@ -32,10 +13,19 @@ public:
             vector<int> arr(m, 0);
             
             for(int i=k; i<n; i++){
+                
+                unordered_map<int,int> mp;
+                mp[0] = 1;
+                int sum = 0;
+                
                 for(int j=0; j<m; j++){
                     arr[j] += matrix[i][j];
+                    sum += arr[j];
+                    if(mp.find(sum-target) != mp.end()){
+                        cnt += mp[sum-target];
+                    }
+                    mp[sum]++;
                 }
-                cnt += subarraySum(arr, target);
             }
         }
         return cnt;
