8191194d9f8d951d5251aa3aa2d7e5694a0ea28d
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-01-29 21:54:17 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (9.02%) - LeetHub

--- 0761-special-binary-string/0761-special-binary-string.cpp
+++ 0761-special-binary-string/0761-special-binary-string.cpp
@@ -11,29 +11,24 @@ public:
         vector<string> ans;
         
         for(int i=0; i<n; i++){
+            
             cnt += s[i]=='1';
             cnt -= s[i]=='0';
             str += s[i];
+            
             if(cnt == 0){
-                if(str.size() == n){
-                    str.erase(0, 1);
-                    if(str.size()) str.pop_back();
-                }
-                // if(s.size() == 8) cout<<str<<' ';
-                ans.push_back(f(str));
+                
+                str.erase(0, 1);
+                str.pop_back();
+                
+                ans.push_back("1" + f(str) + "0");
                 str = "";
             }
         }
         sort(ans.rbegin(), ans.rend());
-        // for(auto i: ans) cout<<i<<' ';
-        // cout<<'\n';
+        
         str = "";
         for(auto i: ans) str += i;
-        if(ans.size() == 1){
-            str = "1" + str;
-            str += "0";
-        }
-        // cout<<str<<' ';
         return str;
     }
     
