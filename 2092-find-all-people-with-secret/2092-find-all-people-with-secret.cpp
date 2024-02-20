8051be6cbfc20515734e9d9aeb651af9cd5a1543
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-02-20 20:15:13 +0530

    Time: 903 ms (29.05%), Space: 264 MB (17.85%) - LeetHub

---
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -0,0 +1,47 @@
+class Solution{
+public:
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+        
+        int m = meetings.size();
+        vector<pair<int,int>> adj[n];
+        
+        for(int i=0; i<m; i++){    
+            int v = meetings[i][0];
+            int u = meetings[i][1];
+            int x = meetings[i][2];
+            adj[v].push_back({ u, x });
+            adj[u].push_back({ v, x });
+        }
+        
+        queue<pair<int,int>> q;
+        q.push({ 0, 0 });
+        q.push({ firstPerson, 0 });
+        
+        set<int> st;
+        set<vector<int>> vis;
+        vector<int> dis(n, 1e9);
+        
+        while(!q.empty()){
+            
+            int node = q.front().first;
+            int time = q.front().second;
+            st.insert(node);
+            q.pop();
+            
+            for(auto i: adj[node]){
+                
+                if(i.second >= time and vis.count({ node, i.first, i.second }) == 0 and
+                    vis.count({ i.first, node, i.second }) == 0
+                    and dis[i.first] >= i.second){
+                    
+                    dis[i.first] = i.second;
+                    vis.insert({ node, i.first, i.second });
+                    vis.insert({ i.first, node, i.second });
+                    q.push({ i.first, i.second });
+                }
+            }
+        }
+        vector<int> ans (st.begin(), st.end());
+        return ans;
+    }
+};
