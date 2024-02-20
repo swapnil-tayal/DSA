3261d0d951d3e52c8325d7be625ee43c47a81103
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-02-20 09:54:27 +0530

    Time: 10 ms (88.91%), Space: 20.4 MB (32.85%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int n = nums.size();
+        int sum = 0;
+        for(auto &i: nums) sum += i;
+        return ((n)*(n+1)/2) - sum;
+    }
+};
