cac9972909b710c29bc69a416c5a8ecb486015f8
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-02-21 09:54:31 +0530

    Time: 8 ms (53.91%), Space: 8.7 MB (46.79%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        
+        if(left == right) return left;
+        
+        bitset<32> it1(left);
+        bitset<32> it2(right);
+        
+        int lastSet1, lastSet2;
+        for(int i=0; i<32; i++){
+            if(it1[i] == 1) lastSet1 = i;
+            if(it2[i] == 1) lastSet2 = i;
+        }
+        if(lastSet1 != lastSet2) return 0;
+        else{
+            int ans = 0;
+            for(int i=lastSet2; i>=0; i--){
+                if(it1[i] == 1 and it2[i] == 1){
+                    ans += (1<<i);
+                }else if(it1[i] != it2[i]) break;
+            }
+            return ans;
+            return 0;
+        }
+    }
+};
