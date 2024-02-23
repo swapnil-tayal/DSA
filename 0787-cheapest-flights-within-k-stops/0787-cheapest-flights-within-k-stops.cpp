11ea19bb1de8f4ffa4d62a57b57d2b0092469cce
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-02-23 09:56:16 +0530

    Time: 36 ms (15.26%), Space: 20.5 MB (7.74%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -2,38 +2,41 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
+        vector<pair<int,int>> adj[n];
+        for(auto i: flights){
+            
+            int v = i[0];
+            int u = i[1];
+            int dis = i[2];
+            adj[v].push_back({ u, dis });
+        }
+        
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
         pq.push({ 0, src, k+1 });
         
-        vector<pair<int,int>> adj[n];
-        for(auto i: flights){
-            adj[i[0]].push_back({ i[1], i[2] });
-        }
-        vector<vector<int>> dis(n, vector<int>(k+2, 1e9));
-        
-        dis[src][k+1] = 0;
+        vector<vector<int>> dis(n, vector<int>(k+1, 1e9));
+        dis[src][k] = 0;
         
         while(!pq.empty()){
             
-            int node = pq.top()[1];
             int dist = pq.top()[0];
+            int node = pq.top()[1];
             int leftK = pq.top()[2];
             pq.pop();
-            // cout<<node<<' '<<leftK<<'\n';
-            
-            if(leftK >= 0 and node == dst) return dist;
-            if(leftK < 0) continue;
             
             for(auto i: adj[node]){
                 
-                if(dis[i.first][leftK] > i.second + dist){
-                // if(vis[i.first][leftK-1]) continue;
-                // vis[i.first][leftK-1] = 1;
-                    dis[i.first][leftK] = i.second + dist;
-                    pq.push({ dis[i.first][leftK], i.first, leftK-1 });
+                if(leftK-1 < 0) continue; 
+                if(dis[i.first][leftK-1] > i.second + dist){
+                    dis[i.first][leftK-1] = i.second + dist;
+                    pq.push({ dis[i.first][leftK-1], i.first, leftK-1 });
                 }
             }
         }
-        return -1;
+        int ans = 1e9;
+        for(int i=0; i<=k; i++){
+            ans = min(ans, dis[dst][i]);
+        }
+        return ans == int(1e9) ? -1 : ans;
     }
 };
