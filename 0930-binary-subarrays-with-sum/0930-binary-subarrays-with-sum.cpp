20814a27ceb03dfdf34ec505444597d71c31141a
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-03-14 10:14:06 +0530

    Time: 67 ms (20.45%), Space: 38 MB (38.55%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        
+        int n = nums.size();
+        map<int,int> mp;
+        mp[0] = 1;
+        int sum = 0;
+        int cnt = 0;
+        
+        for(int i=0; i<n; i++){
+            
+            sum += nums[i];
+            if(mp.find(sum-goal) != mp.end()) cnt += mp[sum-goal];
+            mp[sum]++;
+        }
+        return cnt;
+    }
+};
