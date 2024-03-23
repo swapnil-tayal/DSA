fe7ebf34dd4b4559b4664a020e7bf391425d8504
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-03-23 08:51:32 +0530

    Time: 1023 ms (5.04%), Space: 21.3 MB (43.65%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* rev(ListNode* head){
+        
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next;
+        while(curr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    void reorderList(ListNode* head) {
+        
+        ListNode* temp = head;
+        while(temp->next){
+            temp->next = rev(temp->next);
+            temp = temp->next;
+        }
+    }
+};
