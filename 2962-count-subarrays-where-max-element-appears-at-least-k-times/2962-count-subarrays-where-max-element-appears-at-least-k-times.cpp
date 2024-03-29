a4cddbba68b6869d6075eb8ffe2133af55e25565
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-03-29 08:51:08 +0530

    Time: 123 ms (80.62%), Space: 120 MB (84.30%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -4,21 +4,25 @@ public:
         
         int maxi = *max_element(nums.begin(), nums.end());
         int n = nums.size();
-        int i = 0;
+        
         int j = 0;
-        long long cnt = 0;
+        int i = 0;
+        int cntMaxi = 0;
+        
         long long ans = 0;
         while(j < n){
             
-            cnt += (nums[j] == maxi);
-            if(cnt < k) j++;
-            else{
+            cntMaxi += (nums[j] == maxi);
+            if(cntMaxi < k){
+                j++;
+            }else{
                 
-                while(cnt >= k){
+                while(i<n and cntMaxi >= k){
                     ans += n-j;
-                    cnt -= (nums[i] == maxi);
+                    cntMaxi -= (nums[i] == maxi);
                     i++;
-                }j++;
+                }
+                j++;
             }
         }
         return ans;
