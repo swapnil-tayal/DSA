9837af46d46855fcff6999698b495883b861254e
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-03-30 10:35:02 +0530

    Time: 113 ms (48.41%), Space: 49.8 MB (56.66%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    int atMost(int k, vector<int> &nums){
+        
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+        unordered_map<int,int> mp;
+        int cnt = 0;
+        
+        while(j < n){
+            
+            mp[nums[j]]++;
+            while(mp.size() > k){
+                mp[nums[i]]--;
+                if(mp[nums[i]] == 0) mp.erase(nums[i]);
+                i++;
+            }
+            if(mp.size() <= k) cnt += j-i+1;
+            j++;
+        }
+        return cnt;
+    }
+    
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        
+        return atMost(k, nums) - atMost(k-1, nums);
+    }
+};
