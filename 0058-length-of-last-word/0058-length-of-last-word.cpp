7da33caa9e9550a06beffe18bb230450914cd0e3
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-04-01 19:16:22 +0530

    Time: 4 ms (47.87%), Space: 7.6 MB (57.50%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans = 0;
+        
+        for(int i=s.length()-1; i>=0; i--){
+            
+            if(ans > 0 && s[i]==' '){
+                return ans;
+            }
+            if(s[i]!=' '){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
