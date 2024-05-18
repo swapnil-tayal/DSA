ac1f10ff955dbfea0f013a5ecc37d76951afaa98
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-05-18 11:19:09 +0530

    Time: 5 ms (39.35%), Space: 15.3 MB (100.00%) - LeetHub

--- 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
@@ -10,22 +10,21 @@
  * };
  */
 class Solution{
-    int ans = 0;
 public:
     
-    int f(TreeNode* root){
+    int f(TreeNode* root, int &ans){
         
         if(!root) return 0;
-        
-        int left = f(root->left);
-        int right = f(root->right);
+        int right = f(root->right, ans);
+        int left = f(root->left, ans);
         ans += abs(left) + abs(right);
-        return root->val + left + right - 1;
+        return root->val + right + left - 1;
     }
     
     int distributeCoins(TreeNode* root) {
         
-        f(root);
+        int ans = 0;
+        f(root, ans);
         return ans;
     }
 };
