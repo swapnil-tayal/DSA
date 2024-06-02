27505700f47ef9d5f9f9458348f9e800c49f287c
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-06-02 10:45:42 +0530

    Time: 358 ms (66.67%), Space: 107.4 MB (16.67%) - LeetHub

---
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        
+        int n = s.size();
+        map<char,vector<int>> mp;
+        
+        char ch = 'z';
+        for(int i=0; i<n; i++){
+            
+            if(s[i] != '*'){
+                mp[s[i]].push_back(i);
+                ch = min(ch, s[i]);
+            }
+            else{
+                s[mp[ch].back()] = '.';
+                mp[ch].pop_back();
+                if(mp[ch].size() == 0){
+                    mp.erase(ch);
+                    ch = 'z';
+                    for(auto &k: mp) ch = min(ch, k.first);
+                }
+            }
+        }
+        string ans = "";
+        for(auto i: s){
+            if(i == '*' || i == '.') continue;
+            ans += i;
+        }
+        return ans;
+    }
+};
