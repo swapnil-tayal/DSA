4dfbe98fe729bb3f351db1d8c80ec9133ae8fa91
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-06-06 22:01:18 +0530

    Time: 154 ms (34.91%), Space: 53.4 MB (61.16%) - LeetHub

---
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& hand, int groupSize) {
+        map<int,int> mp;
+        for(auto i: hand){
+            mp[i]++;
+        }
+        
+        while(mp.size()){
+            
+            int mini = mp.begin()->first;
+            int cnt = 0;
+            for(int i=0; i<groupSize; i++){
+                
+                
+                if(mp.find(mini+i) == mp.end()){
+                    return false;
+                }else{
+                
+                    mp[mini+i]--;
+                    if(mp[mini+i] == 0) mp.erase(mini+i);
+                    cnt++;
+                }
+            }
+            if(cnt != groupSize) return false;
+        }
+        return true;   
+    }
+};
