358eb148e4239366372b10989bc19aa18c780b81
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-06-08 12:12:21 +0530

    Time: 257 ms (8.95%), Space: 135.2 MB (14.02%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -5,37 +5,15 @@ public:
         long long n = nums.size();
         if(n == 1) return 0;
         
-        vector<long long> pre(n);
-        pre[0] = nums[0];
+        map<int,int> mp;
+        mp[0] = 0;
         
-        for(long long i=1; i<n; i++){
-            pre[i] += pre[i-1] + nums[i];
-        }
-        
-        map<long long,long long> mp;
-        mp[0] = -1;
         long long sum = 0;
-        
-        for(long long i=0; i<n; i++){
+        for(int i=0; i<n; i++){
             
             sum += nums[i];
-            sum = sum % k;
-            if(mp.find(sum) != mp.end()){
-                
-                long long sumCur = pre[i];
-                // cout<<i<<' '<<mp[sum]<<'\n';
-                
-                if(mp[sum] != -1){
-                    
-                    if(i-mp[sum] < 2) continue; 
-                    sumCur -= pre[mp[sum]];
-                
-                }else if(i < 1) continue;
-                
-                // cout<<sum<<'\n';
-                if(sumCur % k == 0) return 1;
-            }
-            mp[sum] = i;
+            if(!mp.count(sum % k)) mp[sum % k] = i+1;
+            else if(mp[sum % k] < i) return 1;
         }
         return 0;
     }
