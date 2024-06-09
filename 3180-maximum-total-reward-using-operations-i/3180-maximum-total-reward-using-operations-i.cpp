f5fa079abe680b10a39388e39401e500c7123e87
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-06-09 11:42:15 +0530

    Time: 802 ms (20.00%), Space: 385.3 MB (60.00%) - LeetHub

---
+++ 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    int ans = 0;
+public:
+    
+    void f(int i, vector<int> &arr, int sum, vector<vector<int>> &dp){
+        
+        if(i == arr.size()){
+            ans = max(ans, sum);
+            return;
+        }
+        
+        if(dp[i][sum]) return;
+        
+        if(sum < arr[i]) f(i+1, arr, sum+arr[i], dp);
+        f(i+1, arr, sum, dp);
+        
+        dp[i][sum] = 1;
+        return;
+    }
+    
+    int maxTotalReward(vector<int>& arr) {
+        
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> dp(n, vector<int>(8000, 0));
+        int sum = 0;
+        f(0, arr, sum, dp);
+        return ans;
+    }
+};
