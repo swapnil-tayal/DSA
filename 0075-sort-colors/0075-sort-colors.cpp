a97681f735b3aba3d9c4252b116ecce0252679a8
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-06-12 21:57:56 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (34.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         
+        
         int i = 0;
         int j = 0;
         int n = nums.size();
         
         while(i < n){
-            
             if(nums[i] == 0){
                 swap(nums[i], nums[j]);
                 j++;
@@ -16,12 +16,12 @@ public:
         }
         i = j;
         while(i < n){
-            
             if(nums[i] == 1){
                 swap(nums[i], nums[j]);
                 j++;
             }
             i++;
         }
+        return;
     }
 };
