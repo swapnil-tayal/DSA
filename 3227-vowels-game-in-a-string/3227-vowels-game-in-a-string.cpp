166710f32e56fde6f92eb447b2cd93d6de6d0b1d
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-07-21 10:37:24 +0530

    Time: 40 ms (100.00%), Space: 17.5 MB (20.00%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        
+        for(auto &i: s){
+            if(i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u'){
+                return 1;
+            }
+        }
+        return 0;
+    }
+};
