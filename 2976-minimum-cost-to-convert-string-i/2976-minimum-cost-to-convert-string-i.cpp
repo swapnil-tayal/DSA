758abea4323b297ac6b4d69e618e73fe25529287
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-07-27 19:59:35 +0530

    Time: 1682 ms (5.11%), Space: 114.8 MB (30.85%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
-    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        
-        
-        int n = original.size();
-        vector<vector<long long>> grid(26, vector<long long>(26, 1e11));
-        
-        for(int i=0; i<n; i++){
-            grid[original[i]-'a'][changed[i]-'a'] = min(grid[original[i]-'a'][changed[i]-'a'], (long long)cost[i]);
-        }
-        
-        for(int k=0; k<26; k++){
-            for(int i=0; i<26; i++){
-                for(int j=0; j<26; j++){
-                    grid[i][j] = min(grid[i][j], (long long)grid[i][k] + (long long)grid[k][j]);
+    void solve(int n,vector<int> &vis, vector<vector<long long>> &vec,long long sum,vector<long long> & dis) {
+        for(int i=0;i<26;i++){
+            if(i==n || vis[i]==1) continue;
+            if(vec[n][i]!=-1){
+                if(sum+vec[n][i]<dis[i]){
+                    vis[i]=1;
+                    dis[i] = sum+vec[n][i];
+                    solve(i,vis,vec,dis[i],dis);
+                    vis[i]=0;
                 }
             }
         }
-        for(int i=0; i<26; i++) grid[i][i] = 0;
-        
-        long long ans = 0;
-        for(int i=0; i<source.size(); i++){
-            
-            if(grid[source[i]-'a'][target[i]-'a'] != 1e11){
-                ans += grid[source[i]-'a'][target[i]-'a'];
-                
-            }else return -1;
+        return ;
+    }
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        vector<vector<long long>> vec(26,vector<long long>(26,1e11));
+        vector<vector<long long>> adj;
+        for(int i=0;i<original.size();i++){
+            int a = original[i]-'a';
+            int b = changed[i]-'a';
+            vec[a][b] = min(vec[a][b], (long long)cost[i]);
         }
-        return ans;
+        for(int i=0;i<26;i++){
+            vector<int> vis(26,0);
+            vis[i]=1;
+            vector<long long> dis(26,1e18);
+            solve(i,vis,vec,0,dis);
+            adj.push_back(dis);
+        }
+        long long ans = 0;
+        for(int i=0;i<source.size();i++){
+            int a = source[i]-'a';
+            int b = target[i]-'a';
+            if(a==b) continue;
+            if(adj[a][b]>=1e18) return -1;
+            ans +=adj[a][b];
+        }
+        return ans >= 1e11 ? -1 : ans ;
     }
 };
