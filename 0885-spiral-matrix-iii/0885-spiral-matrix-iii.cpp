174b6734c52120f509d184f8176c641f9bc3e87e
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-08-08 20:34:34 +0530

    Time: 36 ms (5.04%), Space: 33.2 MB (5.04%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
@@ -1,56 +1,34 @@
 class Solution {
+    int n,m;
 public:
-
-    int check(vector<vector<int>> &grid){
-        int n = grid.size();
-        int m = grid[0].size();
-        for(int i=1; i<n-1; i++){
-            for(int j=1; j<m-1; j++){
-                if(grid[i][j] == 1) return 0;
-            }
-        }
-        return 1;
-    }
     
-    void f(int i, int j, string dir, vector<vector<int>> &grid, vector<vector<int>> &ans){
+    void f(int i, int j, string dir, vector<vector<int>> &grid, vector<vector<int>> &ans, int cnt){
         
-        if(check(grid)) return;
-        
-        
-        int n = grid.size();
-        int m = grid[0].size();
         if(i != n-1 && i != 0 && j != m-1 && j != 0){
-            // cout<<i<<' '<<j<<'\n';
+            cnt++;
             grid[i][j] = 2;
             ans.push_back({ i-1, j-1 });
         }
-        // cout<<i<<' '<<j<<' '<<dir<<'\n';
+        if(cnt == ((n-2)*(m-2))) return;
         
         if(dir == "left"){
             
-            if(grid[i+1][j] == 1) f(i+1, j, "down", grid, ans);
-            else f(i, j+1, "left", grid, ans);
+            if(grid[i+1][j] == 1) f(i+1, j, "down", grid, ans, cnt);
+            else f(i, j+1, "left", grid, ans, cnt);
         
         }else if(dir == "down"){
             
-            if(grid[i][j-1] == 1) f(i, j-1, "right", grid, ans);
-            else f(i+1, j, "down", grid, ans);
+            if(grid[i][j-1] == 1) f(i, j-1, "right", grid, ans, cnt);
+            else f(i+1, j, "down", grid, ans, cnt);
         
         }else if(dir == "right"){
-                        
-//             if(i == 3 and j == 5){
-//                 for(auto i: grid){
-//                     for(auto j: i) cout<<j<<' ';
-//                     cout<<'\n';
-//                 }
-//             }
             
-            if(grid[i-1][j] == 1) f(i-1, j, "up", grid, ans);
-            else f(i, j-1, "right", grid, ans);
+            if(grid[i-1][j] == 1) f(i-1, j, "up", grid, ans, cnt);
+            else f(i, j-1, "right", grid, ans, cnt);
             
         }else{
-            if(grid[i][j+1] == 1) f(i, j+1, "left", grid, ans);
-            else f(i-1, j, "up", grid, ans);
+            if(grid[i][j+1] == 1) f(i, j+1, "left", grid, ans, cnt);
+            else f(i-1, j, "up", grid, ans, cnt);
             
         }
         return;
@@ -60,20 +38,14 @@ public:
         
         vector<vector<int>> grid(n+2, vector<int>(m+2, 1));
         
-        n = grid.size();
-        m = grid[0].size();
-        // for(auto i: grid){
-        //     for(auto j: i){
-        //         cout<<j<<' ';
-        //     }
-        //     cout<<'\n';
-        // }
-        // cout<<'\n';
+        this->n = grid.size();
+        this->m = grid[0].size();
         vector<vector<int>> ans;
         ans.push_back({ rStart, cStart });
         grid[rStart+1][cStart+1] = 2;
-        f(rStart+1, cStart+2, "left", grid, ans);
         
+        int cnt = 1;
+        f(rStart+1, cStart+2, "left", grid, ans, cnt);
         return ans;
     }
 };
