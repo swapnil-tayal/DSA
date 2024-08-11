6440d2d5dceca9bda1b942d3ec778803dc38f5a1
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-08-11 10:55:32 +0530

    Time: 178 ms (50.00%), Space: 58.3 MB (41.67%) - LeetHub

---
+++ 3250-find-the-count-of-monotonic-pairs-i/3250-find-the-count-of-monotonic-pairs-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    int f(int i, int prev, vector<int> &nums, vector<vector<int>> &dp){
+    
+        if(i == nums.size()) return 1;
+        if(dp[i][prev+1] != -1) return dp[i][prev+1];
+        
+        int val = nums[i];
+        int cnt = 0;
+        
+        for(int k=0; k<=val; k++){
+            
+            if(prev == -1) cnt = (cnt + f(i+1, k, nums, dp)) % int(1e9 + 7);
+            else{
+                if(k >= prev and val-k <= nums[i-1]-prev){
+                    cnt = (cnt + f(i+1, k, nums, dp)) % (int)(1e9+7);
+                }
+            }
+        }
+        return dp[i][prev+1] = cnt;
+    }
+    
+    int countOfPairs(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(52, -1));
+        return f(0, -1, nums, dp);
+    }
+};
