fb3c9d8c1562469e68ff436f02a1499dd1310026
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-09-24 20:34:48 +0530

    Time: 241 ms (80.18%), Space: 274.2 MB (7.45%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,28 +1,85 @@
+struct Node{
+    
+    Node* links[26];
+    bool flag = false;
+    bool containsKey(char ch){
+        return links[ch-'0'] != NULL;
+    }
+    void put(char ch, Node* node){
+        links[ch-'0'] = node;
+        return;
+    }
+    Node* get(char ch){
+        return links[ch-'0'];
+    }
+    void setEnd(){
+        flag = true;
+    }
+    bool isEnd(){
+        return flag;
+    }
+};
+
+class Trie {
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* node = root;
+        for(int i=0; i<word.size(); i++){
+            if(!node->containsKey(word[i])){
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+        }
+        node->setEnd();
+    }
+    
+    bool search(string word) {
+        Node* node = root;
+        for(int i=0; i<word.size(); i++){
+            if(!node->containsKey(word[i])){
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        return node->isEnd();
+    }
+    
+    int f(string s){
+        
+        Node* node = root;
+        int n = s.size();
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            if(node->containsKey(s[i])){
+                cnt++;
+                node = node->get(s[i]);
+            }else break;
+        }
+        return cnt;
+    }
+    
+};
+
 class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
         
-        set<string> st;
+        Trie *trie = new Trie();
         for(auto &i: arr1){
-            
-            string str = to_string(i);
-            string s = "";
-            for(auto &k: str){
-                s += k;
-                st.insert(s);
-            }
+            string s = to_string(i);
+            trie->insert(s);
         }
+        
         int ans = 0;
         for(auto &i: arr2){
-            
-            string str = to_string(i);
-            string s = "";
-            for(auto &k: str){
-                s += k;
-                if(st.count(s)) ans = max(ans, (int)s.size());
-            }
+            string s = to_string(i);
+            ans = max(ans, trie->f(s));
         }
         return ans;
-        
     }
 };
