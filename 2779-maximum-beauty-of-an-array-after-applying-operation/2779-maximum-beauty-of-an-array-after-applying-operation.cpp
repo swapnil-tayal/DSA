0ec3fa4790dda04ef000569cbdcf07752e633f24
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-12-11 23:39:26 +0530

    Time: 892 ms (5.44%), Space: 303.9 MB (5.17%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -3,7 +3,7 @@ public:
     int maximumBeauty(vector<int>& nums, int k) {
         
         int n = nums.size();
-        vector<int> line(400001, 0);
+        vector<int> line(300002, 0);
         
         for(auto &i: nums){
             
@@ -11,10 +11,10 @@ public:
             line[100000 + i + k + 1]--;
 
         }
-        vector<int> sum(400001, -1);
+        vector<int> sum(300002, -1);
         sum[0] = line[0];
         int ans = line[0];
-        for(int i=1; i<400001; i++){
+        for(int i=1; i<300002; i++){
             sum[i] = sum[i-1] + line[i];
             ans = max(ans, sum[i]);
         }
