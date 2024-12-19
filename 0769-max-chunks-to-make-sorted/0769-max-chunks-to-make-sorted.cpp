b8ef401aff4e4494daeaa961544f32830d0e08da
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-12-19 21:12:48 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (7.32%) - LeetHub

---
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        
+        int n = arr.size();
+        vector<int> a(arr.begin(), arr.end());
+        sort(a.begin(), a.end());
+    
+        map<int, set<int>> mp;
+        for(int i=0; i<n; i++){
+            mp[arr[i]].insert(i);
+        }
+        
+        int maxInd = -1;
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            
+            if(i > maxInd) cnt++;
+            
+            int num = a[i];
+            int ind = *mp[num].begin();
+            mp[num].erase(ind);
+            
+            if(ind > maxInd) maxInd = ind;
+            else{
+                // fuck off
+            }
+        }
+        return cnt;
+    }
+};
