c2fe318bf05b92af32d2329c407313c1b0083043
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-01-11 12:17:13 +0530

    Time: 21 ms (17.13%) | Memory: 15.2 MB (21.68%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        
+        int n = s.size();
+        map<char,int> mp;
+
+        for(auto &i: s) mp[i]++;
+        int cnt = 0;
+        for(auto &i: mp){
+            if(i.second % 2 == 1) cnt++;
+        }
+        return (s.size() >= k) && (cnt <= k);
+    }
+};
