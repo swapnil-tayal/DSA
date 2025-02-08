96d5c5b198557660e2cc50a45e5947c1059e4c88
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-02-08 10:35:01 +0530

    Time: 167 ms (8.44%) | Memory: 198 MB (9.06%) - LeetSync

---
+++ 2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -0,0 +1,32 @@
+class NumberContainers {
+    map<int,set<int>> mp;
+    map<int,int> mp2;
+public:
+    NumberContainers() {
+        
+    }
+    
+    void change(int index, int number) {
+        int prevNum = -1;
+        if(mp2.find(index) != mp2.end()){
+            int prevNum = mp2[index];
+            mp[prevNum].erase(index);
+            if(mp[prevNum].size() == 0) mp.erase(prevNum);
+        }
+        mp[number].insert(index);
+        mp2[index] = number;
+    }
+    
+    int find(int number) {
+        if(mp.find(number) != mp.end()){
+            return *mp[number].begin();
+        }else return -1;
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
