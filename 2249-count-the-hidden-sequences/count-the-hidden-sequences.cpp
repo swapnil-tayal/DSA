385b149a16aa754f4bb44a9dd1f7f5658730dd40
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-04-21 12:19:43 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (75.00%) - LeetSync

--- 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
@@ -5,13 +5,13 @@ public:
         long long mini = 0;
         long long maxi = 0;
         int n = diff.size();
-        vector<long long> arr(n+1);
-        arr[0] = 0;
+        long long last = 0;
         
         for(int i=0; i<n; i++){
-            arr[i+1] = arr[i] + diff[i];
-            mini = min(mini, arr[i+1]);
-            maxi = max(maxi, arr[i+1]);
+            long long curr = last + diff[i];
+            mini = min(mini, curr);
+            maxi = max(maxi, curr);
+            last = curr;
         }
         int d1 = upper-lower;
         int d2 = maxi-mini;
