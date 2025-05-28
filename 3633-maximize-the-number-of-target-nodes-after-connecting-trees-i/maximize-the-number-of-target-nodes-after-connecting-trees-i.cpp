99ef5e52e5ae970d2a4b0705d0bf5673785cbea3
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-05-28 21:30:53 +0530

    Time: 154 ms (70.05%) | Memory: 61.8 MB (63.59%) - LeetSync

---
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+
+    void f(int src, int par, vector<vector<int>> &adj, int &cnt, int k){
+        if(k <= 0) return;
+        for(auto i: adj[src]){
+            if(i == par) continue;
+            cnt++;
+            f(i, src, adj, cnt, k-1);
+        }
+        return;
+    }
+
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
+        
+        int m = edges2.size() + 1;
+        int n = edges1.size() + 1;
+        vector<int> inD(m);
+        vector<vector<int>> adj2(m);
+        vector<vector<int>> adj1(n);
+        for(auto i: edges2){
+            adj2[i[0]].push_back(i[1]);
+            adj2[i[1]].push_back(i[0]);
+            inD[i[0]]++;
+            inD[i[1]]++;
+        }
+        for(auto i: edges1){
+            adj1[i[0]].push_back(i[1]);
+            adj1[i[1]].push_back(i[0]);
+        }
+        int maxid = 0;
+        for(int i=0; i<m; i++){
+            if(inD[maxid] < inD[i]) maxid = i;
+        }
+        int maxCnt = 0;
+        for(int i=0; i<m; i++){
+            int cnt = (k>0) ? 1: 0;
+            f(i, -1, adj2, cnt, k-1);
+            maxCnt = max(maxCnt, cnt);
+        }
+        vector<int> ans;
+        for(int i=0; i<n; i++){   
+            int currCnt = 1;
+            f(i, -1, adj1, currCnt, k);
+            cout<<currCnt<<' ';
+            ans.push_back(currCnt + maxCnt);
+        }
+        return ans; 
+    }
+};
