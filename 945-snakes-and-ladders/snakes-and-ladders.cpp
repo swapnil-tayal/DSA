efc50fa18ba813539f8b1c4d3787775919315c4a
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-05-31 12:45:42 +0530

    Time: 19 ms (17.23%) | Memory: 20.6 MB (13.15%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.cpp
+++ 945-snakes-and-ladders/snakes-and-ladders.cpp
@@ -1,84 +1,55 @@
 class Solution {
-    
-    unordered_map<int, int> mp;
-    public:
-    
-    void createMap(vector<vector<int>> &board){
-        
+    map<int,int> mp;
+public:
+
+    void makeMap(vector<vector<int>> &board){
         int n = board.size();
-        int x = n*n;
-        
-        for(int i=0; i<n and n%2 == 0 ; i++){
-            
-            for(int j=0; j<n and i%2==0; j++){
-                mp[x--] = board[i][j];
-            }
-            for(int j=n-1; j>=0 and i%2!=0; j--){
-                mp[x--] = board[i][j];
-            }
-        }
-        for(int i=0; i<n and n%2 != 0 ; i++){
-            
-            for(int j=0; j<n and i%2!=0; j++){
-                mp[x--] = board[i][j];
-            }
-            for(int j=n-1; j>=0 and i%2==0; j--){
-                mp[x--] = board[i][j];
-            }
+        int var = 1;
+        int cnt = 1;
+        for(int i=n-1; i>=0; i--){
+            if(var == 1){
+                for(int j=0; j<n; j++) mp[cnt++] = board[i][j];
+            }else{
+                for(int j=n-1; j>=0; j--) mp[cnt++] = board[i][j];    
+            }var = 1 - var;
         }
     }
-    
-    int bfs(int n){
-        
-        // return -1;
-        queue<pair<int, int>> q;
-        q.push({1, 0});
-        vector<int> vis(n*n+1, -1);
-        
-        while(!q.empty()){
-            
-            int val = q.front().first;
-            int steps = q.front().second;
-            q.pop();
-            vis[val] = 1;
-            // cout<<val;
-            
-            if(val == n*n){
-                return steps;
-            }
-            
-            for(int i=val+1; i <= min(val+6, n*n); i++){
-                
-                if(vis[i] == -1){
-                    
-                    vis[i] = 1;
-                    
-                    if(mp[i] != -1){
 
-                        if(mp[i] >= n*n) return steps+1;
-                        q.push({mp[i], steps+1});
-                    }else{
+    int bfs(vector<vector<int>> &board){
+        
+        int n = board.size();
+        vector<int> vis(n*n+1);
+        queue<int> q;
+        q.push(1);
+        vis[1] = 1;
+        int steps = 0;
 
-                        if(i >= n*n) return steps+1;
-                        q.push({i, steps+1});
+        while(q.size()){
+
+            int k = q.size();
+            while(k--){
+
+                int ind = q.front();
+                q.pop();
+                if(ind == n*n) return steps;
+
+                for(int i=1; i<=6; i++){
+                    int next = ind + i;
+                    if(next>n*n) continue;
+                    int dest = mp[next] != -1 ? mp[next] : next;
+                    if(!vis[dest]){
+                        vis[dest] = 1;
+                        q.push(dest);
                     }
                 }
             }
+            steps++;
         }
         return -1;
     }
-    
+
     int snakesAndLadders(vector<vector<int>>& board) {
-        
-        int n = board.size();
-        if(n == 13 and board[0][5] == 144) return 6;
-        if(n == 15 and board[0][5] == 196) return 4;
-        if(n == 17 and board[0][4] == 191) return 6;
-        if(n == 17 and board[0][2] == 136) return 5;
-        if(n == 18 and board[0][3] == 100) return 7;
-        if(n == 19 and board[0][16] == 310) return 8;
-        
-        createMap(board);
-        return bfs(n);
+        makeMap(board);
+        return bfs(board);
     }
 };
