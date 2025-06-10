bdd11a7a6c1e2baf046085bfddc9c82226220bc0
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-06-10 21:43:50 +0530

    Time: 2 ms (44.05%) | Memory: 9.4 MB (68.21%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int maxDifference(string s) {
-        
-        map<int,int> mp;
-        for(auto i: s) mp[i]++;
-        vector<int> maxO;
-        vector<int> maxE;
-        for(auto &i: mp){
-            if(i.second % 2) maxO.push_back(i.second);
-            else maxE.push_back(i.second);
+        int odd=-1;
+        int even=INT_MAX;
+        vector<int> frq(26);
+        for(char c:s) frq[c-'a']++;
+        for(int i=0;i<26;i++){
+            if(frq[i]%2) odd=max(odd,frq[i]);
+            else if(frq[i]) even=min(even,frq[i]);
         }
-        int ans = -1e9;
-        for(auto i: maxE){
-            for(auto j: maxO){
-                ans = max(ans, j - i);
-            }
-        }
-        return ans;
+        return odd-even;
     }
 };
