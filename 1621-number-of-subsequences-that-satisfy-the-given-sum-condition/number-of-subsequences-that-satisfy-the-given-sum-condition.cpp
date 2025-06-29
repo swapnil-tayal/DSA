3ca4831d2db1ef8538375142a12a4894d2f03c30
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-06-29 11:27:44 +0530

    Time: 69 ms (5.15%) | Memory: 119.6 MB (5.93%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -20,14 +20,13 @@ public:
             int x = target - nums[i];
             if(x < nums[i]) break;
 
-            int ind = lower_bound(nums.begin(), nums.end(), x) - nums.begin();
-            if(ind == n) ind--;
-            if(nums[ind] > x) ind--;
-            int h = nums[ind];
-            while(ind < n and nums[ind] == h){
-                ind++;
-            }
+            int ind = upper_bound(nums.begin(), nums.end(), x) - nums.begin();
             ind--;
+            // int h = nums[ind];
+            // while(ind < n and nums[ind] == h){
+            //     ind++;
+            // }
+            // ind--;
             ans = (ans + pow[ind-i]) % mod;
         }
         return ans;
