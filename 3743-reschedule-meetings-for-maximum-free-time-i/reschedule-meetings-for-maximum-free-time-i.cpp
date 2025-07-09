75aeef5f1a415a7664da9de8e5dc176507c56552
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-07-09 20:06:14 +0530

    Time: 783 ms (7.62%) | Memory: 126 MB (18.81%) - LeetSync

---
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
+        
+        vector<int> gaps;
+        int prev = 0;
+        for(int i=0; i<startTime.size(); i++){
+            gaps.push_back(startTime[i] - prev);
+            prev = endTime[i];
+        }
+        gaps.push_back(eventTime - prev);
+        for(auto i: gaps){
+            cout<<i<<' ';
+        }
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        int ans = 0;
+        while(j < gaps.size()){
+
+            sum += gaps[j];
+            if(j-i+1 < k+1) j++;
+            else{
+                ans = max(ans, sum);
+                sum -= gaps[i];
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
