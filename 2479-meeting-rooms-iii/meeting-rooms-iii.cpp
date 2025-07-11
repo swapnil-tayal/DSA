07c8dddf245f1ad3ddc02ce76245674348ca18db
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-07-11 20:32:32 +0530

    Time: 95 ms (45.28%) | Memory: 98.3 MB (56.92%) - LeetSync

---
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        
+        int m = meetings.size();
+        vector<long long> room(n, 0);
+        vector<long long> freq(n, 0);
+        sort(meetings.begin(), meetings.end());
+        
+        for(int i=0; i<m; i++){
+            
+            long long roomNo = 1e9;
+            long long currStart = meetings[i][0];
+            for(int k=0; k<n; k++){
+                if(room[k] <= currStart){
+                    roomNo = k;
+                    break;
+                }
+            }
+            if(roomNo != 1e9){
+                room[roomNo] = meetings[i][1];
+                freq[roomNo]++;
+            }
+            else{
+                long long minTime = LONG_MAX;
+                for(int k=0; k<n; k++){
+                    if(room[k] < minTime){
+                        minTime = room[k];
+                        roomNo = k;
+                    }
+                }
+                room[roomNo] += meetings[i][1] - meetings[i][0];
+                freq[roomNo]++;
+            }
+        }
+        long long maxFreq = *max_element(freq.begin(), freq.end());
+        int ind = find(freq.begin(), freq.end(), maxFreq) - freq.begin();
+        return ind;
+    }
+};
