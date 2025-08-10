1688dfcba97be175d066820413747f95e2b5e67e
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-08-10 10:28:20 +0530

    Time: 1 ms (39.23%) | Memory: 8.9 MB (62.83%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,32 +1,17 @@
 class Solution {
-    vector<int> p;
-    map<int,int> mpn;
 public:
-
-    int f(int r){
-
-        for(auto &i: p){
-            string str = to_string(i);
-            if(str.size() == r){
-                map<int,int> mpi;
-                for(auto k: str) mpi[k]++;
-                for(auto k: mpn){
-                    mpi[k.first] -= k.second;
-                    if(mpi[k.first] == 0) mpi.erase(k.first);
-                }
-                if(mpi.size() == 0) return 1;
-            }
-        }
-        return 0;
-    }
 
     bool reorderedPowerOf2(int n) {
         
+        string strN = to_string(n);
+        sort(strN.begin(), strN.end());
         for(int a=1; a<(1e9); a = a*2){
-            p.push_back(a);
+            
+            string str = to_string(a);
+            sort(str.begin(), str.end());
+            if(str == strN) return 1;
+
         }
-        string str = to_string(n);
-        for(auto i: str) mpn[i]++;
-        return f(str.size());
+        return 0;
     }
 };
