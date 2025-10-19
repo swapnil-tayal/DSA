b8cac1de7553c5d301f7f1676292e5f42765e8b3
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-10-19 19:50:47 +0530

    Time: 37 ms (85.40%) | Memory: 11.2 MB (87.17%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string findLexSmallestString(string s, int a, int b) {
-        cout<<s.size();
+
         string e, o;
         for(int i=0; i<s.size(); i++){
             if(i%2 == 0) e += s[i];
@@ -24,24 +24,20 @@ public:
                 ve.push_back(str);
             }
         }
-        string res(100, '9');
+        string res(s.size(), '9');
         for(auto s1: vo){
             for(auto s2: ve){
                 string str = "";
                 int i = 0;
                 int j = 0;
-                // cout<<s1<<' '<<s2<<' ';
                 for(int k=0; k<s.size()/2; k++){
                     str += s2[k];
                     str += s1[k];
                 }
-                // cout<<str<<'\n';
                 for(int k=0; k<s.size(); k++){
-                    // cout<<str<<'\n';
                     rotate(str.rbegin(), str.rbegin() + b, str.rend());
                     res = min(res, str);
                 }
-                // cout<<'\n';
             }
         }
         return res;
