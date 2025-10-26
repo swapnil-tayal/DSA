fe236f1e26f9566def9611c0d3dc5d1962e8a1b9
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-10-26 10:25:42 +0530

    Time: 88 ms (5.32%) | Memory: 147.2 MB (8.93%) - LeetSync

---
+++ 2169-simple-bank-system/simple-bank-system.cpp
@@ -0,0 +1,41 @@
+class Bank {
+    int n;
+    map<int, long long> mp;
+public:
+    Bank(vector<long long>& balance) {
+        n = balance.size();
+        for(int i=0; i<n; i++){
+            mp[i+1] = balance[i];
+        }
+    }
+    
+    bool transfer(int a1, int a2, long long money) {
+        if(a1 > n || a2 > n) return false;
+        if(mp[a1] < money) return false;
+        mp[a2] += money;
+        mp[a1] -= money;
+        return true;
+
+    }
+    
+    bool deposit(int account, long long money) {
+        if(account > n) return false;
+        mp[account] += money;
+        return true;
+    }
+    
+    bool withdraw(int account, long long money) {
+        if(account > n) return false;
+        if(mp[account] < money) return false;
+        mp[account] -= money;
+        return true;
+    }
+};
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
