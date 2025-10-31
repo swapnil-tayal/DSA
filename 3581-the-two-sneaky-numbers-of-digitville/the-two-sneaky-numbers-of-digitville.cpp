e82688d71ec70f713b9e3a3aca88d5f3123a57af
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-10-31 19:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 25.3 MB (75.61%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        vector<int> ans;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for(int i=1; i<n; i++){
+            if(nums[i] == nums[i-1]){
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
