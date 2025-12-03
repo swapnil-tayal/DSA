cd4397a45fedb4a2cc09b7944563566f9bc7f3fe
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2025-12-03 16:47:58 +0530

    Time: 93 ms (45.60%) | Memory: 184.3 MB (93.33%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
@@ -20,16 +20,12 @@ public:
         int n = arr.size();
         vector<long long> pre(n);
         long long ans = 0;
-        // int res = 0;
-        // for(auto i: arr) cout<<i<<' ';
-        // cout<<'\n';
         for(int i=n-1; i>=0; i--){
             long long x = arr[i];
             x = x * (x-1) / 2;
             if(i+1 < n) ans = (ans + (x * pre[i+1])) % int(1e9+7);
             pre[i] = x;
             if(i+1 < n) pre[i] += pre[i+1];
-            // res += ans;
         }
         return ans;
     }
