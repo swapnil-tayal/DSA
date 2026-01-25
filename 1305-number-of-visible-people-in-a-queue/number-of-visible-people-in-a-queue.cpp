1e6a73bfdf14d28cde87c9823f8440327ed41240
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-01-26 00:39:20 +0530

    Time: 12 ms (72.06%) | Memory: 93.1 MB (5.31%) - LeetSync

--- 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.cpp
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.cpp
@@ -3,24 +3,18 @@ public:
     vector<int> canSeePersonsCount(vector<int>& a) {
         
         int n = a.size();
-        stack<pair<int,int>> st;
+        stack<int> st;
         vector<int> ans;
 
         for(int i=n-1; i>=0; i--){
-
             int cnt = 0;
-            int check = 0;
-            while(st.size() and st.top().first < a[i]){
-                // check = 1;
+            while(st.size() and st.top() < a[i]){
                 cnt++;
                 st.pop();
             }
-            // cnt += check;
-            if(st.size()){
-                cnt++;
-            }
+            if(st.size()) cnt++;
             ans.push_back(cnt);
-            st.push({ a[i], cnt });
+            st.push(a[i]);
         }
         reverse(ans.begin(), ans.end());
         return ans;
