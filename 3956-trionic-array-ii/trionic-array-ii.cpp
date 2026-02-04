a38d580dd746889896258e8b9e20955fc96dd553
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-02-04 23:19:05 +0530

    Time: 272 ms (6.36%) | Memory: 256.6 MB (7.27%) - LeetSync

---
+++ 3956-trionic-array-ii/trionic-array-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    vector<vector<long long>> dp;
+public:
+
+    long long f(int i, int stage, vector<int> &nums){
+
+        if(i == nums.size()){
+            if(stage == 3) return 0;
+            else return -1e15;
+        }
+        if(dp[i][stage] != -1) return dp[i][stage];
+        long long ans = -1e15;
+        if(stage == 0){
+            if(i-1 >= 0 and nums[i-1] < nums[i]){
+                ans = max(ans, nums[i-1] + nums[i] + f(i+1, 1, nums));
+            }
+            ans = max(ans, f(i+1, 0, nums));
+        }else if(stage == 1){
+            if(nums[i-1] < nums[i]){
+                ans = max(ans, nums[i] + f(i+1, 1, nums));
+            } else if(nums[i-1] > nums[i]){
+                ans = max(ans, nums[i] + f(i+1, 2, nums));
+            } 
+        }else if(stage == 2){
+            if(nums[i-1] > nums[i]){
+                ans = max(ans, nums[i] + f(i+1, 2, nums));
+            } else if(nums[i-1] < nums[i]){
+                ans = max(ans, nums[i] + f(i+1, 3, nums));
+                ans = max(ans, (long long) nums[i]);
+            }
+        }else if(stage == 3){
+            if(nums[i-1] < nums[i]){
+                ans = max(ans, nums[i] + f(i+1, 3, nums));
+            } else ans = 0;
+            // ans = max(ans, f(i, 0, 4, nums));
+        }
+        return dp[i][stage] = ans;
+    }
+
+    long long maxSumTrionic(vector<int>& nums) {
+        
+        dp.resize(nums.size(), vector<long long>(5, -1));
+        return f(0, 0, nums);
+    }
+};
