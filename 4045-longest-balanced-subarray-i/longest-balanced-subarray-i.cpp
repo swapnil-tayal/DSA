9f7f9c6e4b0b66085e4f7e4934c3d3fc873d8a67
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-02-10 21:30:30 +0530

    Time: 449 ms (96.02%) | Memory: 35.3 MB (100.00%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -5,14 +5,15 @@ public:
         int ans = 0;
         int n = nums.size();
         for(int i=0; i<n; i++){
-            set<int> st1;
-            set<int> st2;
+            int arr[100001] = {0};
+            int cnt = 0;
             for(int j=i; j<n; j++){
-                if(nums[j] % 2) st1.insert(nums[j]);
-                else st2.insert(nums[j]);
-                if(st1.size() == st2.size()){
-                    ans = max(ans, j-i+1);
+                if(arr[nums[j]] == 0) {
+                    arr[nums[j]] = 1;
+                    if(nums[j] % 2) cnt++;
+                    else cnt--;
                 }
+                if(cnt == 0) ans = max(ans, j-i+1);
             }
         }
         return ans;
