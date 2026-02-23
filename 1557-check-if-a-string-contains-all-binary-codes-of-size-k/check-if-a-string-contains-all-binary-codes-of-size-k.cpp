fb7d3e44f61c0d3a23ca04050b09e51e0d3d4a80
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-02-23 19:41:00 +0530

    Time: 423 ms (55.61%) | Memory: 119.1 MB (59.96%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,29 +1,13 @@
 class Solution {
-    unordered_set<string> st;
 public:
-
-    int f(int i, int k, string &str){
-
-        if(i == k) return st.count(str);
-        
-        str += "1";
-        int one = f(i+1, k, str);
-        str.pop_back();
-        if(!one) return false;
-        str += "0";
-        int zero = f(i+1, k, str);
-        str.pop_back();
-        return one and zero;
-    }
 
     bool hasAllCodes(string s, int k) {
         
-        string str = "";
         if(k > s.size()) return 0;
+        unordered_set<string> st;
         for(int i=0; i<s.size()-k+1; i++){
             st.insert(s.substr(i, k));
         }
-        // for(auto i: st) cout<<i<<' ';
-        return f(0, k, str);
+        return st.size() == pow(2, k);
     }
 };
