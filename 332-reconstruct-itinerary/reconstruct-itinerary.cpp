13ca10f9335b0cc964566d9c771011886d8e220d
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-02-28 17:28:57 +0530

    Time: 11 ms (28.89%) | Memory: 21 MB (52.42%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -1,30 +1,40 @@
 class Solution {
+    map<string, priority_queue<string, vector<string>, greater<>>> mp;
+    vector<string> ans;
 public:
 
-    // void f()
+    void f(string src){
+
+        while(mp[src].size()){
+            string str = mp[src].top();
+            mp[src].pop();
+            f(str);
+        }
+        ans.push_back(src);
+    
+    }
 
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         
         int n = tickets.size();
-        map<string, priority_queue<string, vector<string>, greater<>>> mp;
         for(auto &i: tickets){
             mp[i[0]].push(i[1]);
         }
-        stack<string> st;
-        vector<string> ans;
-        st.push("JFK");
+        // stack<string> st;
+        // st.push("JFK");
 
-        while(!st.empty()){
+        // while(!st.empty()){
 
-            string str = st.top();
-            if(mp[str].size() == 0){
-                ans.push_back(str);
-                st.pop();
-            }else{
-                st.push(mp[str].top());
-                mp[str].pop();
-            }
-        }
+        //     string str = st.top();
+        //     if(mp[str].size() == 0){
+        //         ans.push_back(str);
+        //         st.pop();
+        //     }else{
+        //         st.push(mp[str].top());
+        //         mp[str].pop();
+        //     }
+        // }
+        f("JFK");
         reverse(ans.begin(), ans.end());
         return ans;
     }
