86e357ff8e9a112505c17e3bb9d9c675fbf93dc7
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-03-08 21:17:23 +0530

    Time: 131 ms (42.50%) | Memory: 223.7 MB (27.25%) - LeetSync

--- 4192-minimum-cost-to-partition-a-binary-string/minimum-cost-to-partition-a-binary-string.cpp
+++ 4192-minimum-cost-to-partition-a-binary-string/minimum-cost-to-partition-a-binary-string.cpp
@@ -2,19 +2,17 @@ class Solution {
     vector<int> one, zero;
 public:
 
-    long long f(string s, int i, int j, int encCost, int flat){
+    long long f(int i, int j, int encCost, int flat){
 
-        int n = s.size();
+        int n = (j-i+1);
         int ones = one[j] - (i-1 >= 0 ? one[i-1] : 0);
         long long cost = 1e16;
         if(ones > 0){
             cost = 1ll * (j-i+1) * ones * encCost;
         } else cost = flat;
 
-        // cout<<s<<' '<<cost<<'\n';
-
         if(n % 2 == 0){
-            cost = min(cost, f(s.substr(0, n/2), i, j-(n/2), encCost, flat) + f(s.substr(n/2), i+(n/2), j, encCost, flat));
+            cost = min(cost, f(i, j-(n/2), encCost, flat) + f(i+(n/2), j, encCost, flat));
         }
         return cost;
     }
@@ -31,6 +29,6 @@ public:
             one.push_back(cnt1);
             zero.push_back(cnt0);
         }
-        return f(s, 0, n-1, encCost, flatCost);
+        return f(0, n-1, encCost, flatCost);
     }
 };
