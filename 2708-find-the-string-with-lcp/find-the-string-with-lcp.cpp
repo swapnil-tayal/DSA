8776419a692e295b7d5498de7399ac4eb5b42289
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-03-28 11:38:38 +0530

    Time: 146 ms (7.23%) | Memory: 126.8 MB (6.02%) - LeetSync

---
+++ 2708-find-the-string-with-lcp/find-the-string-with-lcp.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+
+    bool verifyLcp(string &s, vector<vector<int>> &lcp){
+
+        int n = s.size();
+        vector<vector<int>> a(n, vector<int>(n, 0));
+        for(int i=n-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                if(s[i] == s[j]){
+
+                    if(i+1 < n and j+1 < n) a[i][j] = 1 + a[i+1][j+1];
+                    else a[i][j] = 1;   
+
+                }else{
+                    a[i][j] = 0;
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(a[i][j] != lcp[i][j]) return 0;
+            }
+        }
+        return 1;
+    }
+
+    string findTheString(vector<vector<int>>& lcp) {
+
+        int n = lcp.size();
+        vector<char> ans(n, '-');
+        char ch = 'a';
+
+        for(int i=0; i<n; i++){
+            set<int> st;
+            for(int j=i; j<n; j++){
+                if(lcp[i][j] > 0){
+                    st.insert(i);
+                    st.insert(j);
+                }
+            }
+            char alreadyCh = '-';
+            for(auto k: st){
+                if(ans[k] != '-') alreadyCh = ans[k];
+            }
+            char newCh = alreadyCh == '-' ? ch++ : alreadyCh;
+            if(!(newCh >= 'a' and newCh <= 'z')) return "";
+            for(auto k: st){
+                ans[k] = newCh;
+            }
+        }
+        string res = "";
+        for(auto i: ans) res += i;
+        if(verifyLcp(res, lcp) == false) return ""; 
+        return res;
+    }
+};
