4ecc6c2e1358a425c3dfc171da49d85896e435b5
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-04-12 11:44:04 +0530

    Time: 403 ms (5.22%) | Memory: 152.1 MB (7.23%) - LeetSync

--- 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -15,13 +15,14 @@ public:
 
     int findDis(char ch1, char ch2){
 
+        if(ch1 == '-') return 0;
         auto a1 = mp[ch1];
         auto a2 = mp[ch2];
         return abs(a1[0] - a2[0]) + abs(a1[1] - a2[1]);
 
     }
 
-    int f(int i, int isF, int isS, char lastF, char lastS, string word){
+    int f(int i, char other, string word){
 
         if(i == word.size()){
             return 0;
@@ -29,30 +30,20 @@ public:
 
         string str = to_string(i);
         str += '-';
-        str += to_string(isF);
-        str += '-';
-        str += to_string(isS);
-        str += '-';
-        str += lastF;
-        str += '-';
-        str += lastS;
+        str += to_string(other);
 
         if(dp.find(str) != dp.end()) return dp[str];
 
-        int ans = 1e9;
-        if(!isF) ans = f(i+1, 1, isS, word[i], lastS, word);
-        else {
+        char curr = (i > 0) ? word[i-1] : '-';
 
-            ans = findDis(lastF, word[i]) + f(i+1, isF, isS, word[i], lastS, word);
-            if(!isS) ans = min(ans, f(i+1, isF, 1, lastF, word[i], word));
-            else ans = min(ans, findDis(lastS, word[i]) + f(i+1, isF, isS, lastF, word[i], word));
-        
-        }
-        return dp[str] = ans;
+        int ans1 = findDis(curr, word[i]) + f(i+1, other, word);
+        int ans2 = findDis(other, word[i]) + f(i+1, curr, word);
+
+        return dp[str] = min(ans1, ans2);
     }
 
     int minimumDistance(string word) {
         
-        return f(0, 0, 0, '-', '-', word);
+        return f(0, '-', word);
     }
 };
