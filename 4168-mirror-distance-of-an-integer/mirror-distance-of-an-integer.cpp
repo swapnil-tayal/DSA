1936a515908b48eeb77de7ed68270888b87b6a07
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-04-18 10:12:37 +0530

    Time: 1 ms (8.60%) | Memory: 8.6 MB (30.08%) - LeetSync

---
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        
+        int rev = 0;
+        int temp = n;
+        while(temp > 0){
+            rev = rev * 10 + (temp % 10);
+            temp = temp / 10;
+        }
+        return abs(rev - n);
+    }
+};
