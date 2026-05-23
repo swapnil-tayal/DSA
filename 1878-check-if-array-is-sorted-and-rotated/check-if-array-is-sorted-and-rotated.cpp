82f11630c30f0c4731e7dce5fdcf5301ef640a20
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-05-23 11:21:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (53.59%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -3,14 +3,10 @@ public:
     bool check(vector<int>& nums) {
         
         int n = nums.size();
-        int cnt = 0;
-        for(int i=0; i<n-1; i++){
-            cnt += (nums[i] > nums[i+1]);
-            if(cnt == 2) return 0;
+        while(n--){
+            if(is_sorted(nums.begin(), nums.end())) return 1;
+            rotate(nums.begin(), nums.begin() + 1, nums.end());
         }
-        if(cnt == 0) return 1;
-        else{
-            return nums[n-1] <= nums[0];
-        }
+        return 0;
     }
 };
