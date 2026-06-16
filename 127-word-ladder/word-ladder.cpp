e55f6759064161c41f66d573dff34f73ed2f5267
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-06-16 21:13:08 +0530

    Time: 335 ms (25.70%) | Memory: 19.9 MB (82.93%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        queue<string> q;
+        q.push(beginWord);
+        set<string> st;
+        for(auto &i: wordList){
+            st.insert(i);
+        }
+        int cnt = 1;
+        while(!q.empty()){
+
+            int n = q.size();
+            while(n--){
+                string str = q.front();
+                if(str == endWord) return cnt;
+                q.pop();
+                for(int k=0; k<str.size(); k++){
+                    string temp = str;
+                    for(char ch='a'; ch<='z'; ch++){
+                        temp[k] = ch;
+                        if(st.count(temp)){
+                            st.erase(temp);
+                            q.push(temp);
+                        }
+                    }
+                }
+            }
+            cnt++;
+        }
+        return 0;
+    }
+};
