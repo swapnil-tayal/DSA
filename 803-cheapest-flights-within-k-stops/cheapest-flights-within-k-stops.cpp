7883957b38c0b5206061e628524e55297616dee3
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-06-21 21:19:32 +0530

    Time: 4 ms (44.98%) | Memory: 17.6 MB (83.78%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -7,31 +7,27 @@ public:
             adj[i[0]].push_back({ i[1], i[2] });
         }
 
-        vector<vector<int>> dis(n, vector<int>(k+1, 1e9));
+        vector<int> dis(n, 1e9);
         priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
-        pq.push({ 0, src, k+1 });
-        dis[src][k] = 0;
+        pq.push({ 0, src, 0 });
+        dis[src] = 0;
 
         while(!pq.empty()){
 
-            int dist = pq.top()[0];
+            int stops = pq.top()[0];
             int node = pq.top()[1];
-            int stops = pq.top()[2];
+            int cost = pq.top()[2];
             pq.pop();
+            if(stops > k) continue;
 
             for(auto &i: adj[node]){
                 
-                if(stops-1 < 0) continue;
-                if(dis[i.first][stops-1] > i.second + dist){
-                    dis[i.first][stops-1] = i.second + dist;
-                    pq.push({ dis[i.first][stops-1], i.first, stops-1 });
+                if(dis[i.first] > i.second + cost){
+                    dis[i.first] = i.second + cost;
+                    pq.push({ stops+1, i.first, dis[i.first] });
                 }
             }
         }
-        int ans = 1e9;
-        for(auto &i: dis[dst]){
-            ans = min(ans, i);
-        }
-        return ans == 1e9 ? -1 : ans;
+        return dis[dst] == 1e9 ? -1 : dis[dst];
     }
 };
