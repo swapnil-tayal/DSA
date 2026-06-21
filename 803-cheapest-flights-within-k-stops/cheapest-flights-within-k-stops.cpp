6a899bde953d88540357c7fad0838e0123f1e35d
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-06-21 17:50:28 +0530

    Time: 31 ms (6.92%) | Memory: 20.8 MB (7.68%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -3,40 +3,35 @@ public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
         vector<pair<int,int>> adj[n];
-        for(auto i: flights){
-            
-            int v = i[0];
-            int u = i[1];
-            int dis = i[2];
-            adj[v].push_back({ u, dis });
+        for(auto &i: flights){
+            adj[i[0]].push_back({ i[1], i[2] });
         }
-        
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({ 0, src, k+1 });
-        
+
         vector<vector<int>> dis(n, vector<int>(k+1, 1e9));
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+        pq.push({ 0, src, k+1 });
         dis[src][k] = 0;
-        
+
         while(!pq.empty()){
-            
+
             int dist = pq.top()[0];
             int node = pq.top()[1];
-            int leftK = pq.top()[2];
+            int stops = pq.top()[2];
             pq.pop();
-            
-            for(auto i: adj[node]){
+
+            for(auto &i: adj[node]){
                 
-                if(leftK-1 < 0) continue; 
-                if(dis[i.first][leftK-1] > i.second + dist){
-                    dis[i.first][leftK-1] = i.second + dist;
-                    pq.push({ dis[i.first][leftK-1], i.first, leftK-1 });
+                if(stops-1 < 0) continue;
+                if(dis[i.first][stops-1] > i.second + dist){
+                    dis[i.first][stops-1] = i.second + dist;
+                    pq.push({ dis[i.first][stops-1], i.first, stops-1 });
                 }
             }
         }
         int ans = 1e9;
-        for(int i=0; i<=k; i++){
-            ans = min(ans, dis[dst][i]);
+        for(auto &i: dis[dst]){
+            ans = min(ans, i);
         }
-        return ans == int(1e9) ? -1 : ans;
+        return ans == 1e9 ? -1 : ans;
     }
 };
