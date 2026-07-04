e89a341a3dc95544643ce3c95ce4b6ec57728cd2
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-07-04 23:49:09 +0530

    Time: 27 ms (2.27%) | Memory: 8.6 MB (30.02%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        
+        map<char,int> mp;
+        for(auto &i: s){
+            mp[i]++;
+        }
+        int n = s.size();
+        priority_queue<pair<int,int>> pq;
+        for(auto i: mp){
+            pq.push({ i.second, i.first });
+        }
+        s = string(n, '-');
+        int ind = 0;
+        int isDone = 0;
+
+        while(!pq.empty()){
+
+            char ch = pq.top().second;
+            int cnt = pq.top().first;
+            pq.pop();
+
+            while(cnt--){
+                s[ind] = ch;
+                cout<<ind<<' '<<ch<<' ';
+                ind = (ind + 1) % n;
+                if(ind > 0 and s[ind-1] == ch){
+                    ind = (ind+1) % n;
+                }
+                while((cnt || pq.size()) && ind < n and s[ind] != '-'){
+                    ind = (ind+1) % n;
+                }
+                // cout<<ind<<'\n';
+            }
+            // cout<<s<<' '<<ind<<'\n';
+        }
+        for(int i=1; i<n; i++){
+            if(s[i] == s[i-1]) return "";
+        }
+        return s;
+    }
+};
