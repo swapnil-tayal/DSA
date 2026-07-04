abcc9729b28c5ea7cd7bf794ec512495c10bcaa7
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-07-04 18:35:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (28.26%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -22,28 +22,39 @@ public:
 class Solution {
 
     map<int, Node*> mp;
-    map<int, Node*> org;
 public:
 
-    void f(Node* node) {
-        
-        if(!node) return;
-        mp[node->val] = new Node(node->val);
-        org[node->val] = node;
-        for(auto &i: node->neighbors){
-            if(i && mp.find(i->val) != mp.end()) continue;
-            f(i);
+    Node* getClone(int val){
+
+        Node* cloneNode;
+        if(mp.find(val) != mp.end()) cloneNode = mp[val];
+        else{
+            mp[val] = new Node(val);
+            cloneNode = mp[val];
         }
+        return cloneNode;
+    }
+
+    void f(Node* node, vector<int> &vis){
+
+        vis[node->val] = 1;
+        Node* cloneNode = getClone(node->val);
+
+        for(auto &i: node->neighbors){
+
+            Node* cloneChild = getClone(i->val);
+            cloneNode->neighbors.push_back(cloneChild);
+            if(vis[i->val]) continue;
+            f(i, vis);
+        }
+        return;
     }
 
     Node* cloneGraph(Node* node) {
         
-        f(node);
-        for(auto &i: mp){
-            for(auto node: org[i.first]->neighbors){
-                mp[i.first]->neighbors.push_back(mp[node->val]);
-            }
-        }
-        return mp[1];
+        if(node == NULL) return NULL;
+        vector<int> vis(101);
+        f(node, vis);
+        return mp[node->val];
     }
 };
