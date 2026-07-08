e5d13d575fdea24c8528aaa809394db871eb5d2a
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-07-08 23:10:28 +0530

    Time: 2578 ms (5.18%) | Memory: 152.1 MB (73.58%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -16,38 +16,40 @@ public:
         
 
         int n = s.size();
-        vector<long long> num(n+1, 0);
-        vector<long long> sum(n+1, 0);
-        vector<long long> len(n+1, 0);
+        vector<long long> num(n, 0);
+        vector<long long> sum(n, 0);
+        vector<long long> len(n, 0);
 
         long long sm = 0;
         long long nm = 0;
         for(int i=0; i<n; i++){
 
             long long val = s[i] - '0';
-            sum[i+1] = sum[i] + val;
+            sum[i] = (i-1 >= 0 ? sum[i-1] : 0) + val;
 
             if(val != 0){
-                num[i+1] = ((num[i] * 10ll) + val) % mod;
-                len[i+1] = len[i]+1;
+                num[i] = ((i-1 >= 0 ? (num[i-1] * 10ll) : 0) + val) % mod;
+                len[i] = (i-1 >= 0 ? len[i-1] : 0) + 1;
             }
             else{
-                num[i+1] = num[i];
-                len[i+1] = len[i];
+                num[i] = i-1 >= 0 ? num[i-1] : 0;
+                len[i] = i-1 >= 0 ? len[i-1] : 0;
             }
         }
 
         vector<int> ans;
+        for(auto i: num) cout<<i<<' ';
+        cout<<'\n';
 
         for(auto &i: queries){
             
-            int l = i[0];
-            int r = i[1] + 1;
+            int l = i[0]-1;
+            int r = i[1];
 
-            long long s1 = num[l] * pow[len[r] - len[l]] % mod;
+            long long s1 = (l >= 0 ? num[l] : 0) * pow[len[r] - (l >= 0 ? len[l]: 0)] % mod;
             long long s2 = (num[r] - s1 + mod) % mod;
-            ans.push_back(s2 * (sum[r] - sum[l]) % mod);
+            ans.push_back(s2 * (sum[r] - (l >= 0 ? sum[l] : 0)) % mod);
         }
         return ans;
     }
-};
+}; 
