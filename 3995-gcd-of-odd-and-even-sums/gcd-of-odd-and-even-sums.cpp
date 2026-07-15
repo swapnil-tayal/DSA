3eb11df43b0db7df184abe3bde222353c93b11af
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-07-15 12:00:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.02%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        
-        int sum1 = 0;
-        int sum2 = 0;
-        for(int i=1; i<2*n; i++){
-            if(i%2) sum2 += i;
-            else sum1 += i;
-        }
-        return __gcd(sum1, sum2);
+        return n;
     }
 };
