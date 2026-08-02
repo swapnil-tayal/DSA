6602b38a2e18de4d9f9a18b810fd39775ad3763f
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-08-02 11:54:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (46.05%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,27 +1,28 @@
 class Solution {
-    vector<vector<vector<int>>> dp;
+    // vector<vector<vector<int>>> dp;
 public:
     
-    int f(int i, int j, vector<int> &piles, int isA){
+    // int f(int i, int j, vector<int> &piles, int isA){
 
-        if(i > j) return 0;
-        if(dp[i][j][isA] != -1) return dp[i][j][isA];
-        if(isA){
-            int left = piles[i] + f(i+1, j, piles, 0);
-            int right = piles[j] + f(i, j-1, piles, 0);
-            return dp[i][j][isA] = max(left, right);
-        }else{
-            int left = -piles[i] + f(i+1, j, piles, 1);
-            int right = -piles[j] + f(i, j-1, piles, 1);
-            return dp[i][j][isA] = min(left, right);
-        }
-    }
+    //     if(i > j) return 0;
+    //     if(dp[i][j][isA] != -1) return dp[i][j][isA];
+    //     if(isA){
+    //         int left = piles[i] + f(i+1, j, piles, 0);
+    //         int right = piles[j] + f(i, j-1, piles, 0);
+    //         return dp[i][j][isA] = max(left, right);
+    //     }else{
+    //         int left = -piles[i] + f(i+1, j, piles, 1);
+    //         int right = -piles[j] + f(i, j-1, piles, 1);
+    //         return dp[i][j][isA] = min(left, right);
+    //     }
+    // }
     
     bool stoneGame(vector<int>& piles) {
         
-        int n = piles.size();
-        dp.assign(n, vector<vector<int>>(n, vector<int>(2, -1)));
-        return f(0, n-1, piles, 1) > 0;
+        return true;
+        // int n = piles.size();
+        // dp.assign(n, vector<vector<int>>(n, vector<int>(2, -1)));
+        // return f(0, n-1, piles, 1) > 0;
 
     }
 };
