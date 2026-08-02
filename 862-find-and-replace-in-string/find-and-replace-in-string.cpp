444d9ff630711797e4fcbbbe487fd706f66ccad6
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-08-02 21:32:39 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (15.80%) - LeetSync

--- 862-find-and-replace-in-string/find-and-replace-in-string.cpp
+++ 862-find-and-replace-in-string/find-and-replace-in-string.cpp
@@ -3,19 +3,13 @@ public:
     string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
         
         int currOffset = 0;
-        unordered_set<int> seen;
         vector<tuple<int, string, string>> arr;
 
         for (int i = 0; i < indices.size(); i++) {
             if(s.substr(indices[i], sources[i].size()) != sources[i]) continue;
-            if (seen.count(indices[i])) {
-                continue;
-            }
-            seen.insert(indices[i]);
             arr.push_back({indices[i], sources[i], targets[i]});
         }
         sort(arr.begin(), arr.end());
-
         for (int i = 0; i < arr.size(); i++) {
             tie(indices[i], sources[i], targets[i]) = arr[i];
         }
