663c8618018443bb47dd705bae0cead728ff9051
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-08-04 09:12:59 +0530

    Time: 1 ms (60.57%) | Memory: 33.6 MB (28.39%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> ans;
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        set<int> st(nums.begin(), nums.end());
+        unordered_set<int> st(nums.begin(), nums.end());
         for(int i=nums[0]; i<=nums[n-1]; i++){
             if(st.count(i) == 0) ans.push_back(i);
         }   
