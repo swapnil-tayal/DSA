80b8f9b3fbf22dce11a42d656dfd1b55d4c8ea14
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-08-06 22:14:14 +0530

    Time: 495 ms (5.25%) | Memory: 99.2 MB (5.51%) - LeetSync

---
+++ 2279-maximum-split-of-positive-even-integers/maximum-split-of-positive-even-integers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<long long> maximumEvenSplit(long long finalSum) {
+        
+        if(finalSum % 2 == 1) return {};
+    
+        set<long long> st;
+        long long val = 2;
+    
+        while(val < finalSum){
+            if(2*val != finalSum && !st.count(val) and !st.count(finalSum - val)){
+                st.insert(val);
+                finalSum -= val;
+            }
+            val += 2;
+            // cout<<val<<' '<<finalSum<<'\n';
+        }
+        if(finalSum > 0) st.insert(finalSum);
+        vector<long long> a(st.begin(), st.end());
+        return a;
+    }
+};
+
+// 28
+// 2 26
+// 2 4 22
+// 2 4 6 16
+// 2 4 6 16
