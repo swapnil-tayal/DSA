77a0e54ba49b6a38bfe85025128e1a954d52ab6e
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-08-11 11:54:48 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (29.32%) - LeetSync

--- 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -3,7 +3,7 @@ public:
     int missingInteger(vector<int>& nums) {
         
         int n = nums.size();
-        set<int> st;
+        unordered_set<int> st;
         for(auto i: nums) st.insert(i);
         int sum = nums[0];
         for(int i=1; i<n; i++){
