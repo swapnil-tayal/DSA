39fbfbd115b6ce988791abd82e8b66a2744ad2c5
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-08-14 10:09:15 +0530

    Time: 75 ms (5.14%) | Memory: 25.3 MB (5.14%) - LeetSync

---
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+      
+        int n = s.size();
+        int maxi = 0;
+        
+        for(int i=0; i<n; i++){
+            map<char,int> mp;
+            int cnt = 0;
+            for(int j=i; j<n; j++){
+                mp[s[j]]++;
+                cnt++;
+                int f = 0;
+                for(auto &k: mp){
+                    if(k.second > 2) f = 1;
+                }    
+                if(!f) maxi = max(maxi, cnt);
+            }
+        }
+        return maxi;
+    }
+};
