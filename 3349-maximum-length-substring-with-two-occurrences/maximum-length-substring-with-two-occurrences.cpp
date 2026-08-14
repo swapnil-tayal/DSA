ea76e46a1ad985a8100ac75b08771ba3bed16e78
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-08-14 10:09:32 +0530

    Time: 53 ms (5.14%) | Memory: 24.1 MB (5.14%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -6,7 +6,7 @@ public:
         int maxi = 0;
         
         for(int i=0; i<n; i++){
-            map<char,int> mp;
+            unordered_map<char,int> mp;
             int cnt = 0;
             for(int j=i; j<n; j++){
                 mp[s[j]]++;
