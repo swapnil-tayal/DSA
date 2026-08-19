abf26bdb4385f2d4059989de59877cd88d5ea03f
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-08-19 20:28:52 +0530

    Time: 272 ms (5.07%) | Memory: 110.6 MB (5.07%) - LeetSync

---
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& a) {
+        
+        sort(a.begin(), a.end());
+        int ans = 0;
+
+        set<int> st;
+        set<vector<int>> st2;
+        for(auto &i: a){
+            st.insert(i[0]);
+            st2.insert(i);
+        }
+        ans += (n - st.size()) * 2;
+
+        for(auto &i: st){
+
+            if(!st2.count({ i, 2 }) and !st2.count({ i, 3 }) and !st2.count({ i, 4 }) and !st2.count({ i, 5 })){
+                ans++;
+                if(!st2.count({ i, 6 }) and !st2.count({ i, 7 }) and !st2.count({ i, 8 }) and !st2.count({ i, 9 })){
+                    ans++;
+                }
+            }
+            else if(!st2.count({ i, 4 }) and !st2.count({ i, 5 }) and !st2.count({ i, 6 }) and !st2.count({ i, 7 })){
+                ans++;
+            }
+            else if(!st2.count({ i, 6 }) and !st2.count({ i, 7 }) and !st2.count({ i, 8 }) and !st2.count({ i, 9 })){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
