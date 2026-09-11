933de47835c9c454792bb5d5dfce86806af9cfd5
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2026-09-11 20:35:30 +0530

    Time: 378 ms (5.23%) | Memory: 164.8 MB (6.76%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -3,13 +3,13 @@ public:
     int totalNumbers(vector<int>& digits) {
         
         int n = digits.size();
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         for(auto &i: digits) mp[i]++;
         int cnt = 0;
 
         for(int i=100; i<1000; i+=2){
 
-            map<int,int> mp2;
+            unordered_map<int,int> mp2;
             for(auto &k: to_string(i)) mp2[k-'0']++;
             int f = 1;
             for(auto &k: mp2){
